7a8be4c84960eb2a8c82fdc2026afaf3fea95f15
nedyudombat <nedyudombat@gmail.com>
2018-04-29 15:07:44 +0100

    home page

--- index.php
+++ index.php
@@ -2,45 +2,35 @@
 include_once("header.php");
 ?>
 
-<div class="jumbotron jumbotron-fluid text-center bg-white mb-5 pb-5">
+<div class="jumbotron jumbotron-fluid hero-div text-center bg-transparent mb-5 pb-5">
   <p class="hero-main-text font-weight-bold"> hng</p>
   <p class="hero-sub-text-1 font-weight-bold"> Internship</p>
   <p class="hero-sub-text-2 text-primary"> Become a better Software Developer</p>
 </div>
 
-<div class="container container-fluid bg-white text-center mt-5 px-5 pt-5">
-  <h2>What is HNG Internship</h2>
-  <p class="text-secondary mx-5 px-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<div class="container container-fluid bg-transparent text-center mt-5 px-5 pt-5">
+  <h2>What is HNG Internship ?</h2>
+  <p class="text-secondary mx-5 px-5">The HNG is a 3-month remote internship program designed to locate the most talented software developers in Nigeria and the whole of Africa. Everyone is welcome to participate (there is no entrance exam). We create fun challenges every week on our slack channel. THose who solve them stay on. Everyone gets to learn important concepts quickly, and make connections with people they can work with in the future. The intern coders are introduced to complex programming frameworks, and get to work on real applications that scale. the finalist are connected to the best companies in the tech ecosystem and get full time jobs and contracts immediately.</p>
 </div>
 
-<div class="container container-fluid bg-white text-center my-5 px-5 py-5">
-  <h2>Why the HNG Internship</h2>
-  <p class="text-secondary mx-5 px-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<div class="container container-fluid bg-transparent text-center my-5 px-5 py-5">
+  <h2>Why the HNG Internship ?</h2>
+  <p class="text-secondary mx-5 px-5">We needed developers in Nigeria, and there were just not enough. We found that telent was hiding in different small locations all over the country - but they needed training and exposure to best practices. The initial idea was to simply do a remote internship to find coders. We did not expect 1000+ people would apply to the internship. But we saw strongly people wanted to develop their skills, and it became a mission for us to make this happen.</p>
 </div>
 
 <div class="my-5 py-5">
-	<div class="container container-fluid bg-white my-5 py-5">
-		<h2>Ready to get started register now</h2>
-		<div class="d-flex justify-content-center mt-5">
+	<div class="container container-fluid bg-transparent my-5 py-5">
+		<h3 class="text-center">Ready to get started? register now</h3>
+		<div class="d-flex justify-content-center mt-3">
 		  <form class="w-75">
 		  	<label class="mb-0 pb-0">Email</label>
 		  	<div class="input-group mb-4 mt-0">
 		  	  <div class="input-group-prepend ">
 		  	    <span class="input-group-text bg-transparent px-5 font-icon" id="basic-addon1">@</span>
 		  	  </div>
-		  	  <input type="text" class="form-control form-control-lg rounded-right" placeholder="johndoe@example.com" aria-label="Username" aria-describedby="basic-addon1">
-		  	  <a href="https://join.slack.com/t/hnginternship4/shared_invite/enQtMzQwOTU4NzAwNjExLWQ0NWFlZDBmNjRkMTRkNGZmYjQ5MzA0YmUzZDBiZDEzOTBkZGE1ZWUxZTI1YjkxMTQ5N2MyZTMyMzBmMTEyOWM" class="btn btn-primary btn-lg ml-2 rounded">
-		  	  	<h2 class="font-weight-normal text-white">Sign Up</h2>
+		  	  <input type="text" class="form-control  rounded-right bg-transparent" placeholder="johndoe@example.com" aria-label="Username" aria-describedby="basic-addon1">
+		  	  <a href="https://join.slack.com/t/hnginternship4/shared_invite/enQtMzQwOTU4NzAwNjExLWQ0NWFlZDBmNjRkMTRkNGZmYjQ5MzA0YmUzZDBiZDEzOTBkZGE1ZWUxZTI1YjkxMTQ5N2MyZTMyMzBmMTEyOWM" class="btn btn-blue btn-lg ml-3 rounded py-0">
+		  	  	<p class="font-weight-normal text-white f-4 mb-0 pt-2 mt-1 text-capitalize">Sign Up</p>
 		  	  </a>
 		  	</div>
 		  </form>
