aed7c17d091d3e2f11e8deede5211753dcbd8e36
mercyikpe <ikpemercy1@gmail.com>
2018-04-29 19:09:24 +0100

    Added partners.php

---
+++ partners.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   <link href="https://fonts.googleapis.com/css?family=Lato|Work+Sans" rel="stylesheet">
+    <title>Partners</title>
+
+<style>
+    body, html {
+    height: 100%;
+    }
+
+    p {
+       font-size: 14px; 
+    }
+
+    .banner {
+    /* The image used */
+    background-image: url("img/Rectangle.jpg");
+    height: 300px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    }
+    .rightColumn {
+        padding: 100px 5px 5px 80px;
+    }
+
+    ul {
+    list-style-type: circle;
+    }
+
+    .flex-container {
+      display: flex;
+      flex-wrap: wrap;
+      background-color: DodgerBlue;
+    }
+
+    .flex-container > div {
+      background-color: #f1f1f1;
+      width: 100px;
+      margin: 10px;
+      text-align: center;
+      line-height: 75px;
+      font-size: 30px;
+      flex-flow: row wrap;
+      justify-content: space-around;
+    }
+
+</style>
+
+</head>
+
+<body>
+    <div class="container-fluid">        
+      
+      <div class="container-fluid banner">
+          <h1> Hotels.ng</h1>
+      </div>
+
+<div class="row">
+ 
+<div class="container"> 
+
+    <div class="row">
+      <div class="col-sm-3 col-md-6 col-lg-4">
+        <img src="img/overlay.jpg">
+            <button>Visit Website</button>
+            
+            <h3>About</h3>
+            
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+
+    <div class="col-sm-9 col-md-6 col-lg-8 rightColumn">
+
+    <div class="col">
+
+            <img src="img/Rectangle 2.png">
+    </div> 
+
+    <div class="col">
+            
+        <div class="flex-container">
+          <div><h6>Headquarters:</h6><p>Yaba, Lagos</p></div>
+          <div><h6>Industry:</h6><p>Hotel Booking Services</p></div>
+          <div><h6>Address:</h6><p> No 3, Birrel Avenue</p></div>  
+          <div><h6>Phone:</h6><p>0806 621 2033</p></div>
+          <div><h6>Founded:</h6><p>1972</p></div> 
+        </div>
+
+    </div> 
+
+
+    <div>
+        <h3> Contributions</h3>
+
+        
+        <p>Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials</p>
+
+        <ul>
+        <li><span class="glyphicon glyphicon-menu-right"></span>
+            Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials
+        </li>
+        <li><span class="glyphicon glyphicon-menu-right"></span>
+            Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials
+        </li>
+        <li><span class="glyphicon glyphicon-menu-right"></span>
+            Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials
+        </li>
+        </ul>  
+
+          
+
+        </div>
+
+    </div>
+
+
+
+    </div>
+
+  </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
