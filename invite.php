b8ed336fd59272c63ff4aec6975ddf5b29ac0a1e
olamide Faniyan <faniyanolamide@gmail.com>
2018-04-29 17:59:38 +0100

    invite.php

--- invite.php
+++ invite.php
@@ -1,40 +1,46 @@
 <?php
-	/**
-	 * This is a simple script to invite users to your slack
-	 * Replace the subdomain and token in the variables below.
-	 * Upload a logo called "logo.png" to the same directory for your group
-	 * Upload a logo called "slack.png" to the same directory for slack
-	 */
-	define('SUBDOMAIN','{hnginternship4}');
-	define('TOKEN','{YOUR API TOKEN HERE}');
-?>
-
-<?php
 include_once("header.php");
 ?>
+<style type="text/css">
+	#email, #phone, #lastname, #firstname{
+		width: 350px;
+		border-radius: 7px;
+	}
 
-<html>
-	<head>
-		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-	</head>
-	<body style=" width: 100%">
-		<div style="text-align: center; margin-top: 75px">
-			<div>
-				<img src="img/logo.png" style="width: 150px; height: 70px; padding: 10px;" />
-				<img src="img/slack.png" style="width: 150px; height: 70px; padding: 10px;" />
-			</div>
-			<h2 style="font-family: 'Roboto', sans-serif; color: #ffffff">Join <?=SUBDOMAIN?> on Slack!</h2>
+</style>
+<div class="" style="padding-top: 10%">
+    <div class="col-md-6  mx-auto">
+        <h1 class="login-title text-center" style="font-weight: bold; font-size: 50px">Invite to HNG</h1>
+        <p style="font-size: 16px; margin-bottom: 0px; margin-top: 0px; opacity: 0.7" class="text-center">You can send out invites to all your friends all over the world to join<br/> this amazing community
+        </p>
+        <form action="" class="text-center" style="margin-top:">
+            <div class="form-row" style="margin-left: 30px;">
+                <div class="form-group col-md-6">
+                	<p align="left" style="font-size: 12px; margin-bottom: 0px; margin-left: 10px; opacity: 0.7">Firstname</p>
+                    <input type="text" name="firstname" id="firstname" class="form-control" placeholder="">
+                </div>
+                <div class="form-group col-md-6">
+                	<p align="left" style="font-size: 12px; margin-bottom: 0px; margin-left: 10px; opacity: 0.7">lastname</p>
+                    <input type="text" name="lastname" id="lastname" class="form-control" placeholder="">
+                </div>
+            </div>
+            <div class="form-row" style="margin-left: 30px;">
+                <div class="form-group col-md-6" style="margin-left: 0px; ">
+                	<p align="left" style="font-size: 12px; margin-bottom: 0px; margin-left: 10px; opacity: 0.7">Email address</p>
+                    <input type="email" name="email" id="email" class="form-control" placeholder="">
+                </div>
+                <div class="form-group col-md-6">
+                	<p align="left" style="font-size: 12px; margin-bottom: 0px; margin-left: 10px; opacity: 0.7">Phone</p>
+                    <input type="text" name="phone" id="phone" class="form-control" placeholder="">
+                </div>
+            </div>
+                 
+                    <button style="margin-top: 30px; border:0px; margin-bottom: 7px; background-color: #2196F3; color: white; width: 400px; font-size: 12px; height: 40px; border-radius: 10px" id="submitbutton" class="">Send Invite</button>
+        </form>
 
+        </div>
+        </div>
 
-
-        <!-- Please Code the form here -->
-
-
-
-			
-			
-		</div>
-	</body>
-</html>
-
-
+        <?php
+        include_once("footer.php");
+        ?>
