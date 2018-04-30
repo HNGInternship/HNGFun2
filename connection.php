2d6fa646e368047f4f6c2bc29cc0419b8f98b9f2
chibuokem <chibuokemibezim@gmail.com>
2018-04-30 17:07:52 +0100

    connection modified

--- connection.php
+++ connection.php
@@ -2,10 +2,12 @@
 //$db = new MySQLi('localhost','root','','hngfun')
 //$db = new MySQLi('localhost','root','','hng_fun');
 
-define ('DB_USER', "root");
+/*define ('DB_USER', "root");
 define ('DB_PASSWORD', "");
 define ('DB_DATABASE', "hng_fun");
 define ('DB_HOST', "localhost");
+*/
+require once '../config.php'
 
 try {
     $db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
