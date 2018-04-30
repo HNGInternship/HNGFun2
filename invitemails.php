3023e6d1be632a549e99823789429b908ee92cb1
olamide Faniyan <faniyanolamide@gmail.com>
2018-04-30 15:52:23 +0100

    invite page redo

---
+++ invitemails.php
@@ -0,0 +1,21 @@
+<?php
+	/**
+	 * This is a simple script to invite users to your slack
+	 * Replace the subdomain and token in the variables below.
+	 * Upload a logo called "logo.png" to the same directory for your group
+	 * Upload a logo called "slack.png" to the same directory for slack
+	 */
+	define('SUBDOMAIN','hnginternship4');
+	define('TOKEN','xoxp-340958278947-341290703349-356302020102-f943a0e1b43cd95e2ca5abb1b7a29bdb');
+?>
+
+
+<?php
+
+include_once("header.php");
+?>
+
+
+<?php
+    include_once("footer.php");
+?>
