26ec1dcb3220aeeb97c04f05645e6c848f1bbe54
chibuokem <chibuokemibezim@gmail.com>
2018-04-30 03:43:15 +0100

    connection altered

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php 
 //$db = new MySQLi('localhost','root','','hngfun')
-$db = new MySQLi('localhost','root','29gE9t*dJ2#2f-BS','hng_fun');
+$db = new MySQLi('localhost','root','','hng_fun');
 ?>
