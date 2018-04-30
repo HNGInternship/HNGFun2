9a21b7c3d40ed64e3aab7703fbfae3e563261abd
Gwinyai <gtchax@gmail.com>
2018-04-30 14:23:37 +0200

    added login script

--- login.php
+++ login.php
@@ -1,4 +1,133 @@
 <?php
+	session_start(); 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {    
+   
+	if(!defined('DB_USER')){
+		require "config.php";		
+		try {
+			$conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+		} catch (PDOException $pe) {
+			die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+		}
+		}
+		
+	// set constant fot the root url 
+	define("ROOT_URL", $_SERVER['SERVER_NAME']);
+	$username = $_POST['username'] ?? '';
+	$password = $_POST['password'] ?? '';
+	
+
+  // Simple validation
+  if(is_blank($username)) {
+    $errors['username'] = "Username cannot be blank.";
+  }
+
+  if(is_blank($password)) {
+    $errors['password'] = "Password cannot be blank.";
+  }
+
+  //if everything is okay, query DB for username and password
+  if(empty($errors)) {
+	$login_failure = "Log in was unsuccessful.";
+   try {
+	$query = "SELECT * FROM users WHERE (email = :username OR username = :username) AND password = :password";
+		$q = $conn->prepare($query);
+		$q->execute(array(':username' => $username,':password' => $password ));
+        $q->setFetchMode(PDO::FETCH_ASSOC);
+		$data = $q->fetchAll();
+			if(!empty($data)){
+				session_regenerate_id();
+				$_SESSION['authenticated'] = true;
+				$_SESSION['username'] = $data['username'];
+				$_SESSION['last_login'] = time();
+				redirect_to('/listing.php');
+
+			} else {
+				$errors['failed'] = $login_failure;
+		}
+
+      } catch (PDOException $e) {
+        throw $e;
+      }
+  }
+  
+}
+
+
+//Path resolution
+function url_for($path) {
+
+  if($path[0] != '/') {
+    $path = "/" . $path;
+  }
+  return ROOT_URL . $path;
+}
+
+//Set and clear session message
+function get_clear_session() {
+	if(isset($_SESSION['message']) && $_SESSION['message'] != '') {
+		$msg = $_SESSION['message'];
+		unset($_SESSION['message']);
+	}
+}
+
+// display session messages
+function display_session_message() {
+	$msg = get_clear_session();
+	if(!is_blank($msg)) {
+		return '<div id="message">'. h($msg) .'</div>';
+	}
+}
+
+function find_user_by_id($id) {
+  // global conn;
+
+  $sql = "SELECT * from users WHERE user_id = '".$id."' LIMIT 1";
+    try {
+     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+         $query = $conn->query($sql);
+     $user = $query->fetch(PDO::FETCH_OBJ);
+     echo $user;
+    } catch (PDOException $e) {
+        throw $e;
+    }
+}
+ 
+//Check for blank input
+function is_blank($val) {
+  $input = trim($val);
+  if($input === '' || null) {
+    return true;
+  }
+
+  return false;
+}
+
+//Redirect pages
+function redirect_to($location) {
+  header("Location: " . $location);
+  exit;
+}
+
+// Check if user is login in
+function is_logged_in(){
+	return isset($_SESSION['username']);
+}
+
+//Perform all login 
+function login_user($user) {
+
+  session_regenerate_id();
+  // $_SESSION['user_id'] = $user['id'];
+  $_SESSION['authenticated'] = $true;
+  $_SESSION['last_login'] = time();
+
+  return true;
+}
+
+?>
+<?php
 include_once("header.php");
 // require_once("./auth_scripts/login.php");
 ?>
