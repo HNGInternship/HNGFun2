c34d04b27f6c7101c9a652c51f4d2a8870c48340
OvundahKing <ovundahnyenke@gmail.com>
2018-04-30 11:23:51 +0100

    o

---
+++ password-reset-email.php
@@ -0,0 +1,49 @@
+ <?php include("header.php");?>
+<style>
+    .navbar {
+        visibility: hidden;
+    }
+</style>
+
+<div style="text-align: center; margin-top: -50px; padding-bottom: 10px">
+    <h1 class="font-weight-bold" style="color: #2196F3">
+        HNG Internship
+    </h1>
+</div>
+<div class="container" style='color: #3D3D3D;'>
+    <div class="row justify-content-md-center" style="text-align: center">
+        <div class="col-lg-9" style="text-align: justify; text-justify: inter-word; font-size: 90%">
+            <hr/>
+            <h5>Hello John!</h5>
+            <p>
+                You're receiving this email because we received a password reset request for your account.
+                <br/></br/>If that's right, you can enter a new password by clicking on the button below.
+            </p>
+            <a href="resetpassword.php" ><button class="btn btn-primary">Reset Password</button></a>
+            <p>
+                If you did not request a password reset, no further action is required.
+            </p>
+            <p>
+                Regards,
+            </p>
+            <p>
+                HNG Internship
+            </p>
+            <hr/>
+            
+        </div>
+    </div>
+    
+</div>
+
+ <?php include("footer.php");?>
+
+
+
+
+
+
+
+
+
+
