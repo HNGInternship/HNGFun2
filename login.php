94de26266d1bf9a6ecaacc71bab5edaa8340f695
nellybaz10@gmail.com <35724948+nellybaz@users.noreply.github.com>
2018-04-30 18:11:39 +0200

    login update

--- login.php
+++ login.php
@@ -1,143 +1,124 @@
-<?php
-$erros = [];
-$username = '';
-$password = '';
+ <?php include("/header.php");?>
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-	$username = $_POST['username'] ?? '';
-	$password = $_POST['password'] ?? '';
-
-	// Simple validation
-	if(is_blank($username)) {
-		$erros[] = "Username cannot be blank.";
-	}
-
-	if(is_blank($username)) {
-		$erros[] = "Password cannot be blank.";
-	}
-
-	//
-	if(empty($errors)) {
-
-		$user = find_user_by_id($username);
-		$login_failure = "Log in was unsuccessful.";
-		
-		if($user) {
-			
-			if(password_verify($password, $user['hashed_password'])) {
-				// password matches
-				login_user($user);
-				redirect_to(url_for('/dashboard/index.php'));
-			} else {
-				// username found, but password does not match
-				$errors[] = $login_failure;
-			}
-		} else {
-			// no user 
-			$errors[] = $login_failure;
-		}
-	}
-	
-}
-
-function find_user_by_id($id) {
-	// global conn;
-
-	$sql = "SELECT * from users WHERE user_id = '".$id."' LIMIT 1";
-	  try {
-         $query = $conn->query($sql);
-            $user = $query->fetch(PDO::FETCH_OBJ);
-    } catch (PDOException $e) {
-        throw $e;
-    }
-}
- 
-//Check for blank input
-function is_blank($val) {
-	$input = trim($val);
-	if($input === '' || null) {
-		return false;
-	}
-
-	return true;
-}
-
-//Redirect pages
-function redirect_to($location) {
-	header("Location: " . $location);
-	exit;
-}
-
-//Perform all login 
-function login_user($user) {
-
-	session_regenerate_id();
-	$_SESSION['user_id'] = $user['id'];
-	$_SESSION['username'] = $user['username'];
-	$_SESSION['last_login'] = time();
-
-	return true;
-}
-
-?>
-<?php
-include_once("header.php");
-?>
-
-<style>
-	.login-button {
-		background: #2196F3;
-        padding: 0.4em 8em !important;
-        color: white;
-        border-radius: 5px;
-    }
-    .login-button:hover {
-        opacity: 0.8;
-    }
-	.fa.fa-lock {
-		color:#2196F3;
-	}
-	
-	 /* media queries */
-	 @media (max-width: 599px) { 
-		 .login-con {
-			 width: 100% !important;
-		 }
-	 }
-</style>
-
-<div class="container">
-	<div class="d-flex flex-column justify-content-center align-items-center">
-		<div class="d-block w-50 mt-5 login-con">
-			<div class="w-50 m-auto">
-				<h2 class="text-center my-0 py-0" style="margin-bottom: 10px">Log In</h2>
-				<p class="text-center" style="font-size: 15px; opacity: 0.7">Login to access your dashboard and manage your account.</p>
-			</div>
-
-			<form class="w-50 m-auto">
-				<input type="text" name="username" class="form-control mb-3" placeholder="Username or Email" required >
-				<input type="text" name="password" class="form-control mb-3" placeholder="Password" required >
-				<input type="checkbox" name="remember_me" class="" placeholder="Password"><span style="font-size: 14px;"> Remember me</span> 
-				<button class="btn w-100 rounded py-2 login-button">Login</button>
-			</form>
-			<div class="w-50 m-auto text-center">
-				<a href="resetpassword.php">
-					<small><i class="fa fa-lock"></i>&nbsp;&nbsp;Forgotten password? </small>
-				</a>
-			</div>
-			<div class="w-50 m-auto text-center">
-				<small>
-					<span style="color: #c4c4c4">
-						Don't have an account?&nbsp;
-					<span>
-					<a href="signup.php" class="text-primary text-lighter text-center">Get Started</a>
-				</small>
-			</div>
-		</div>
-	</div>
+<div style="text-align: center; padding-top: 20px; padding-bottom: 10px">
+    <h1 class="font-weight-normal">
+        <h1>Log In</h1>
+	    <p style="font-size: 16px;">Login to access your dashboard and manage your account.</p>
+    </h1>
 </div>
 
-<?php
-include_once("footer.php");
-?>
+<div class="container" style='color: #3D3D3D'>
+    <div id="message"></div>
+    <div class="row justify-content-md-center" style="text-align: center">
+        <div class="col-lg-4">
+            <div style="padding: 0px 20px 0px 20px">
+                <form class="form-signin" id="login_form">
+            <label for="inputEmail" class="sr-only">Email</label>
+            <input type="email" id="email" class="form-control" placeholder="Email" name="email" required="" autofocus="">
+            <br/>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
+            <br/>
+            <input type="hidden" name="login" value="yes">
+            <div class="checkbox mb-3" style="text-align: left">
+                <label>
+                    <input type="checkbox" value="remember-me">&nbsp; <span style="font-size: 16px;">Remember me</span>
+                </label>
+            </div>
+            <div>
+               <button class="btn btn-primary btn-block" id="login" type="submit">Log In</button> 
+            </div>
+                </form>
+            </div>
+        <div style="padding-top: 30px; padding-bottom: 30px"> 
+            <img src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698845-icon-118-lock-rounded-128.png" height="15px" width="auto"/>
+            <span style="font-size: 14px; color: grey">Forgot Password?<a href="resetpassword.php" style="color: #008DDD"> Click here</a></span>
+        </div>
+        
+        <div style="padding-bottom: 20px; font-size: 14px; color: #ADADAD">Don't have an account?&nbsp; <a href="signup.php" style="color: #008DDD">Get Started</a></div>
+        </div>
+    </div>
+    
+</div>
+<script type="text/javascript">
+       $( document ).ready(function() {
+    $("#login").click(function(e){
+        e.preventDefault();
+
+       
+        var email = $("#email").val();
+       
+        var password = $("#password").val();
+        
+        
+        if(email ==""){
+            alert('please enter email');
+            $("#message").addClass('alert alert-danger');
+            $("#message").html('Please enter email');
+        }
+       
+        else if(password ==""){
+            alert('Please enter password');
+            $("#message").addClass('alert alert-danger');
+            $("#message").html('Please enter password');
+        }
+
+       
+        else{
+            
+            
+            $("#login").html('Logging in..');
+
+             var data = $("#login_form").serialize();
+
+            
+
+             $.ajax('process.php',{
+            type : 'post',
+            data : data,
+            success: function(data){
+
+             if(data==true){
+                $("#message").addClass('alert alert-success');
+            $("#message").html("Login successful");
+
+            $("#login").html('Redirecting..');
+
+            window.location ="dashboard.php";
+             }  
+             else{
+                alert(data);
+                $("#message").addClass('alert alert-danger');
+            
+                $("#message").html(data);
+                 $("#login").html('Failed');
+             } 
+            
+
+            },
+           error : function(jqXHR,textStatus,errorThrown){
+                 if(textStatus ='error'){
+                    alert('Request not completed');
+                 }
+                $("#login").html('Failed');
+            },
+            beforeSend :function(){
+
+            $("#message").removeClass('alert alert-danger');
+            $("#message").html('');
+
+            $("#login").html('Logging in..');
+            },
+        });
+    
+
+        }
+        
+     });
+
+
+
+    });
+</script>
+ <?php include("/footer.php");?>
