d94863506893b65107a45f034e357678f546a9b8
nellybaz10@gmail.com <35724948+nellybaz@users.noreply.github.com>
2018-04-30 17:31:20 +0200

    update on reset

--- Sign/resetpassword.php
+++ Sign/resetpassword.php
@@ -1,5 +1,5 @@
 <?php
-include_once("header.php");
+include_once("../header.php");
 ?>
 
 
@@ -109,5 +109,5 @@ include_once("header.php");
     })
 </script>
 <?php
-include_once("footer.php");
+include_once("../footer.php");
 ?>
