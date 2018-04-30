4badb3ac52e928c46f3259b20d55f372aba80886
realtech <aiboroma467@users.noreply.github.com>
2018-04-30 11:28:44 +0100

    dreamtech467
    dreamtech467 Update invite page removing the Head and body tag

--- invite.php
+++ invite.php
@@ -936,51 +936,11 @@ $msg = '
         </div>
         </div>
 
+
+
         <?php
         include_once("footer.php");
         ?>
-=======
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>HNG FUN</title>
-
-    <!-- Bootstrap core CSS -->
-      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-
-      <!-- Custom fonts for this template -->
-  <!-- Custom fonts for this template -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-   <!-- Custom styles for this template -->
-   <link href="css/dragonSh.css" rel="stylesheet">
-    <style>
-		
-		.btn{
-			width:300px;
-		}
-	p{
-		font-size: 15px;
-	}
-	 
-    </style>
-
-  </head>
-
-  <body>
 
     <!-- Page Content -->
     <div class="container invite-bd">
@@ -1020,9 +980,7 @@ $msg = '
         </div>
       </div>
     </div>
-  </body>
 
-</html>
 <?php
 include_once("footer.php");
 ?>
