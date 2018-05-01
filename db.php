57d3feda9c10e32f275a6eba2a92229b3e8c6f8d
nellybaz10@gmail.com <35724948+nellybaz@users.noreply.github.com>
2018-05-01 22:09:24 +0200

    update

--- db.php
+++ db.php
@@ -5,7 +5,6 @@ DO NOT MODIFY THIS FILE!!!
 
 /* Instead of rediting this, just move your config.php a step outside the HNGFun folder */
 require 'config_slayers.php';
-// require 'config.php';
 
 // try {
 //     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
