2dca72422f0891c54aa0eefbc4596b4cb13a5c0f
Chigozie <chygoz2@gmail.com>
2018-05-01 14:06:23 +0100

    Removed merge conflict errors

--- buy_coins_0.php
+++ buy_coins_0.php
@@ -1,35 +1,22 @@
 <?php
-
 include_once("coin_header.php");
-
 ?>
 
 <style>
-
 body{
-
     background: #ffffff;
     /*font-size: 14px;*/
     font-family: Lato;
-
-
-
 }
-
 .main-container{
   padding-right: 10%;
     padding-left: 10%;
     font-size: 14px;
-
 }
-
 td,.heavy-text{
     font-weight: 600;
-
 }
-
 main{
-
     margin-bottom: 7.4%;
     /*margin-right: 10%;
     margin-left: 10%;*/
@@ -37,76 +24,54 @@ main{
  box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.25);
  
 }
-
 .input-form{
     padding-top: 11%;
     padding-bottom:4.5%;
 }
-
 .label-for-form,#sellerDetails,thead>tr>th{
     font-size: 1.25em;
     color: #3d3d3d;
     line-height: 1.5em;
-
-
 }
-
 td,.footerText{
      font-size: 1.125em;
     color: #323232;
     line-height:1.250em;
-
 }
-
 #sellerDetails{
     font-weight: bold;
 }
-
 .input-for-form{
-
     font-size: 1.25em;
     color: #828282;
     line-height: 1.5em;
-
 }
-
 .table-div{
     padding-bottom: 5.5%;
 }
-
 .modal-content{
     padding: 1.4% 3.3%;
 }
-
 #modalHeader{
   /*font-size: 1.25em;*/
   /*font-size: 2.625em;*/
   font-size: 2em;
-
   
-
 font-style: normal;
 font-weight: bold;
 line-height: normal;
 color: #3d3d3d;
 margin-bottom: 2.2%;
 }
-
-
 #confirmModal{
-
     background: rgba(196, 196, 196, 0.3) !important;
 }
-
-
 .visible{
     display: block;
 }
-
 .hidden{
     display: none;
 }
-
 /*#scrim{
 background: rgba(0, 0, 0, 0.8);
     color: white;
@@ -118,14 +83,11 @@ background: rgba(0, 0, 0, 0.8);
     margin: 0px;
     /*display: none;*/
 /*}*/
-
  /* media queries */
     @media(min-width: 360px) { 
         #checkMark {
             left:10%;        } 
     }
-
-
   
 </style>
 
@@ -136,24 +98,14 @@ background: rgba(0, 0, 0, 0.8);
 </section> -->
 
 <div class="main-container">
-<<<<<<< HEAD
-<h1 style="text-align: left;color: #3D3D3D;line-height: 1.571em; margin-top: 2.4%;margin-bottom:0%;
-=======
 <h1  style="text-align: left;color: #3D3D3D;line-height: 1.571em; margin-top: 2.4%;margin-bottom:0%;
->>>>>>> 57d006287c50ff3591b2f3b7b4002f6883426e88
 font-family: 'Work Sans';
 font-style: normal;
 font-weight: bold;
 line-height: normal;
-<<<<<<< HEAD
-font-size: 1.429em;"> INPUT DETAILS</h1>
-
-<p style="text-align: left;color: #3D3D3D;font-size:1.286em;line-height: 1.571em; margin: 1.4% 0%;"> Please input the necessary details and crosscheck that they are correct before proceeding</p>
-=======
 font-size: 1.25em;"> INPUT DETAILS</h1>
 
 <p class="heavy-text" style="text-align: left;color: #3D3D3D;font-size:1.125em;line-height: 1.571em; margin: 1.4% 0%;"> Please input the necessary details and crosscheck that they are correct before proceeding</p>
->>>>>>> 57d006287c50ff3591b2f3b7b4002f6883426e88
 
 <main class="container">
     <div class="container">
@@ -257,39 +209,27 @@ font-size: 1.25em;"> INPUT DETAILS</h1>
     
     window.onload = function() {
    
-
     $('#buyButton').on('click', function () {
-
         setTimeout(completeTransaction, 2000);
       
     });
-
-
     
     
    $('#closeButton').on('click', function () {
-
        $('#checkMark').toggleClass('visible');
     $('#checkMark').toggleClass('hidden');
     $('#modalHeader').html('Confirming...');
     $('#modalFooter').toggleClass('hidden');
     $('#modalFooter').toggleClass('visible');
     });
-
-
   }
-
-
   function completeTransaction(){
     $('#checkMark').toggleClass('visible');
     $('#checkMark').toggleClass('hidden');
     $('#modalHeader').html('Transaction Complete');
     $('#modalFooter').toggleClass('hidden');
     $('#modalFooter').toggleClass('visible');
-
-
   }
-
 </script>
 
 <!-- Footer -->
