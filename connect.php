c56af6baa4df842007fa092a55a140161b1bf250
nellybaz10@gmail.com <35724948+nellybaz@users.noreply.github.com>
2018-05-01 13:22:23 +0200

    update

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 include ('config.example.php');
-$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-if(!$conn){
+$db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+if(!$db){
   echo "couldn't connect";
 }
 ?>
