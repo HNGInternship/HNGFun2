2b47a53936c310d47d900dfcdd015bf6cdf703b6
Chigozie <chygoz2@gmail.com>
2018-05-01 14:26:19 +0100

    updated to contain email and phone number

--- slayer_db.php
+++ slayer_db.php
@@ -20,7 +20,6 @@ $sql1 = "CREATE TABLE IF NOT EXISTS `interns_data` (
     `first_name` varchar(100) NOT NULL,
     `last_name` varchar(100) NOT NULL,
     `username` varchar(100) NOT NULL,
-    `username` varchar(100) NOT NULL,
     `email` varchar(100) NOT NULL,
     `phone_number` varchar(30) NOT NULL,
     `skills` text,
