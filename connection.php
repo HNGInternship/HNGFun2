302a2f3e7fca6297e2a43db08ee30ee00cf0c3d5
chibuokem <chibuokemibezim@gmail.com>
2018-05-01 20:24:29 +0100

    connection

--- connection.php
+++ connection.php
@@ -8,7 +8,7 @@ define ('DB_DATABASE', "hngfun");
 define ('DB_HOST', "localhost");
 
 try {
-    $db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
+    $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
 
 } catch (PDOException $pe) {
     die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
