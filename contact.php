3e321809947b0e4f63e62e62a0f420c839b28ede
Gwinyai <gtchax@gmail.com>
2018-05-01 13:50:26 +0200

    banner added to contact page

--- contact.php
+++ contact.php
@@ -2,7 +2,14 @@
 include_once("header.php");
 ?>
 
-
+ <div class="jumbotron bg-cover">
+     <div class="overlay"></div>
+      <div class="container contact">
+        <h1>Get In Touch</h1>
+        <p>Showing up is 80% percent of life</p>
+      
+      </div>
+    </div>
 
 <?php
 include_once("footer.php");
