9a236cc3c576fbe3386dc5714941b25a8e54e475
Somiari Lucky <hello@somiari.me>
2018-05-01 21:04:32 +0100

    Update header.php
    Final fix for header alignment

--- header.php
+++ header.php
@@ -84,14 +84,14 @@
     <!-- Navigation -->
     
     <nav class="navbar navbar-expand-lg navbar-light"  >
-      <a class="navbar-brand" style="padding-left: 50px; href="../index.php">
+      <a class="navbar-brand" style="padding-left: 30px; href="../index.php">
         <img src="img/logo.png" alt="HNG logo" class="img-fluid">
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
     
-      <div class="collapse navbar-collapse" style="padding-right: 50px; id="navbarSupportedContent">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
         
       
         <ul class="navbar-nav ml-auto">
