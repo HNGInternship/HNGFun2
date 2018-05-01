b95f83f8f6d51461273d7bf52b34a82f8d2ea7eb
Okunuga Melody <mokunuga@gmail.com>
2018-05-01 23:40:50 +0100

    Link sell coins pages

--- sell_coins.php
+++ sell_coins.php
@@ -184,15 +184,15 @@ margin-bottom: 2.2%;
   
 <div class="row no-gutters" style="margin-top: 6.5%">
 
-    <div style="position: relative; margin-right: 0%;margin-left: 0%;width: 25%;color: white">
-    <img style="width: 100%" src="img/blue_1_bar.svg" alt="first arrow">
+    <div style="position: relative; margin-right: 0%;margin-left: 0%;width: 31%;color: white">
+        <a href="sell_coins.php"><img style="width: 100%" src="img/blue_1_bar.svg" alt="first arrow"></a>
     <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">1</span>
 
       
     </div>
 
-    <div  style="position: relative; margin-right: 0%;margin-left: 0%;width: 25%;color: white">
-    <img  style="width: 100%" src="img/blue_2_bar.svg" alt="first arrow">
+    <div style="position: relative; margin-right: 0%;margin-left: 0%;width: 31%;color: white">
+        <a href="sell_coins_1.php"><img  style="width: 100%" src="img/blue_2_bar.svg" alt="first arrow"></a>
     <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">2</span>
 
     <img style="width: 20%;height: 100%;position: absolute;top: 0%;left: -2%;padding-left: 0%;" src="img/blue_1_arrow.svg" alt="first arrow">
@@ -200,8 +200,8 @@ margin-bottom: 2.2%;
       
     </div>
 
-    <div style="position: relative; margin-right: 0%;margin-left: 0%;width: 25%;color: white">
-    <img style="width: 100%" src="img/blue_2_bar.svg" alt="first arrow">
+    <div style="position: relative; margin-right: 0%;margin-left: 0%;width: 31%;color: white">
+        <a href="sell_coins_2.php"><img style="width: 100%" src="img/blue_2_bar.svg" alt="first arrow"></a>
     <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">3</span>
 
     <img style="width: 20%;height: 100%;position: absolute;top: 0%;left: -2%;padding-left: 0%;" src="img/blue_2_arrow.svg" alt="first arrow">
@@ -210,15 +210,7 @@ margin-bottom: 2.2%;
       
     </div>
 
-     <div  style="position: relative; margin-right: 0%;margin-left: 0%;width: 25%;color: white">
-    <img style="width: 100%" src="img/blue_2_bar.svg" alt="first arrow">
-    <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">4</span>
 
-    <img style="width: 20%;height: 100%;position: absolute;top: 0%;left: -2%;padding-left: 0%;" src="img/blue_2_arrow.svg" alt="first arrow">
-
-
-      
-    </div>
   </div>
 <h1  style="text-align: left;color: #3D3D3D;line-height: 1.571em; margin-top: 2.4%;margin-bottom:0%;
 font-family: 'Work Sans';
--- sell_coins_2.php
+++ sell_coins_2.php
@@ -34,6 +34,9 @@ main{
     padding-bottom: 20%;
 }
 
+.arrow-head{
+    padding-left: 7%;
+}
 
 
 
@@ -67,44 +70,42 @@ main{
   <div class="item" style="background:green"><a>Carmon Rigg</a></div>
 </div> -->
 
-  
 <div class="row no-gutters" style="margin-top: 6.5%">
 
-    <div style="position: relative; margin-right: 0%;margin-left: 0%;width: 25%;color: white">
-    <img style="width: 100%" src="img/blue_1_bar.svg" alt="first arrow">
-    <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">1</span>
+    <div style="position: relative; margin-right: 0%;margin-left: 0%;width: 31%;color: white">
+        <a href="sell_coins.php"><img style="width: 100%" src="img/blue_1_bar.svg" alt="first arrow"></a>
+        <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">1</span>
 
-      
     </div>
 
-    <div  style="position: relative; margin-right: 0%;margin-left: 0%;width: 25%;color: white">
-    <img  style="width: 100%" src="img/blue_1_bar.svg" alt="first arrow">
+    <div  style="position: relative; margin-right: 0%;margin-left: 0%;width: 31%;color: white">
+        <a href="sell_coins_1.php"><img  style="width: 100%" src="img/blue_1_bar.svg" alt="first arrow"></a>
     <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">2</span>
 
-    <img style="width: 20%;height: 100%;position: absolute;top: 0%;left: -2%;padding-left: 0%;" src="img/blue_1_arrow.svg" alt="first arrow">
+        <img style="width: 20%;height: 100%;position: absolute;top: 0%;left: -2%;padding-left: 0%;" src="img/blue_1_arrow.svg" alt="first arrow">
 
       
     </div>
 
-    <div style="position: relative; margin-right: 0%;margin-left: 0%;width: 25%;color: white">
-    <img style="width: 100%" src="img/blue_1_bar.svg" alt="first arrow">
-    <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">3</span>
+    <div style="position: relative; margin-right: 0%;margin-left: 0%;width: 31%;color: white">
+        <a href="sell_coins_2.php"><img style="width: 100%" src="img/blue_1_bar.svg" alt="first arrow"></a>
+        <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">3</span>
 
-    <img style="width: 20%;height: 100%;position: absolute;top: 0%;left: -2%;padding-left: 0%;" src="img/blue_1_arrow.svg" alt="first arrow">
-
-
-      
-    </div>
-
-     <div  style="position: relative; margin-right: 0%;margin-left: 0%;width: 25%;color: white">
-    <img style="width: 100%" src="img/blue_1_bar.svg" alt="first arrow">
-    <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">4</span>
-
-    <img style="width: 20%;height: 100%;position: absolute;top: 0%;left: -2%;padding-left: 0%;" src="img/blue_1_arrow.svg" alt="first arrow">
+        <img style="width: 20%;height: 100%;position: absolute;top: 0%;left: -2%;padding-left: 0%;" src="img/blue_1_arrow.svg" alt="first arrow">
 
 
       
     </div>
+
+<!--     <div  style="position: relative; margin-right: 0%;margin-left: 0%;width: 25%;color: white">-->
+<!--    <img style="width: 100%" src="img/blue_1_bar.svg" alt="first arrow">-->
+<!--    <span style="position:absolute;top:30%;left: 50%;font-size: 1.253em">4</span>-->
+<!---->
+<!--    <img style="width: 20%;height: 100%;position: absolute;top: 0%;left: -2%;padding-left: 0%;" src="img/blue_1_arrow.svg" alt="first arrow">-->
+<!---->
+<!---->
+<!--      -->
+<!--    </div>-->
   </div>
 <h1  style="text-align: left;color: #3D3D3D;line-height: 1.571em; margin-top: 2.4%;margin-bottom:0%;
 font-family: 'Work Sans';
@@ -114,6 +115,7 @@ line-height: normal;
 font-size: 1.25em;">TRANSACTION COMPLETE</h1>
 
 <p class="heavy-text" style="text-align: left;color: #3D3D3D;font-size:1.125em;line-height: 1.571em; margin: 1.4% 0%;"> You sold 0.1134552442 HNGCoins to Dammy. Your payment has been sent to account number 012345225</p>
+
 
 <main class="container">
     <div class="container">
