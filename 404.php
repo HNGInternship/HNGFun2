569078e1937dfb57fc441fc67c906ba192bce29e
Gwinyai <gtchax@gmail.com>
2018-05-01 11:39:55 +0200

    added 404 page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php
+include_once("header.php");
+?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron page404">
+      <div class="container">
+          <section class="content404">
+        <h1 class="bigtext">404</h1>
+            <p>oops the page you are looking forc can't be found !</p>
+
+                <hr>
+
+                <p><a class="white btn btn-primary btn-lg" href="./index.php">Go to home page</a></p>
+
+          </section>
+      
+      </div>
+    </div>
+
+<?php
+include_once("footer.php");
+?>
--- blog_autosuggest.php
+++ blog_autosuggest.php
@@ -1,6 +1,6 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {    
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {    
    
 	if(!defined('DB_USER')){
 		require "config.php";		
