e3f0f28f4b368c1d79896a64fb77cecd86a9cb39
Gwinyai <gtchax@gmail.com>
2018-05-01 11:55:33 +0200

    added 404 page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php
+include_once("header.php");
+?>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron page404">
+      <div class="container">
+          <section class="content404">
+        <h1 class="bigtext">404</h1>
+            <h4>oops the page you are looking forc can't be found !</h4>
+
+                <hr>
+
+                <p><a class=" btn-404 btn-lg" href="./index.php">Go to home </a></p>
+
+          </section>
+      
+      </div>
+    </div>
+
+<?php
+include_once("footer.php");
+?>
