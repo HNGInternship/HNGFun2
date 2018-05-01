69eba059e7e7a73bdd061c05116be7abc185eda3
Everistus Olumese <everistusolumese@gmail.com>
2018-05-01 06:22:57 +0100

    Added ability for interns to get an account on stellar

---
+++ connection.php
@@ -0,0 +1,9 @@
+if(!defined('DB_USER')){
+        require "./config.php";
+      }
+      try {
+        $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+      } catch (PDOException $pe) {
+        die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+      }
+global $conn;
