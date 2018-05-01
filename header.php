202d6e785ce734c28b5e8982533c0aa051935ab2
nellybaz10@gmail.com <35724948+nellybaz@users.noreply.github.com>
2018-05-01 22:13:48 +0200

    update

--- header.php
+++ header.php
@@ -1,4 +1,29 @@
 
+<?php 
+
+
+
+// for choosing active page on nav bar
+
+$fileName=basename($_SERVER['PHP_SELF']);
+
+$files = array('index.php','learn.php','listing.php','testimonies.php','sponsors.php','alumni.php','partners.php');
+$activeArray = array('','','','','','');
+
+$fileIndex=array_search($fileName,$files);
+
+
+// if page is unknown, dont mark any nav item
+
+if($fileIndex!==FALSE){
+
+$activeArray[$fileIndex]="active";
+}
+
+/////////////////////////////////////////////////////////
+
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,15 +36,16 @@
     <meta name="author" content="">
 
     <title>HNG FUN</title>
-
+    <!--ajax scripts-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <!-- Bootstrap core CSS -->
-      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
 
 
+   <script src="js/jquery.min.js"></script>
       <!-- Custom fonts for this template -->
   <!-- Custom fonts for this template -->
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
@@ -34,12 +60,11 @@
      <!-- <link href="css/learn.css" rel="stylesheet"> -->
 <!--	  <link href="css/carousel.css" rel="stylesheet">-->
       <link href="css/landing-page.min.css" rel="stylesheet">
-      <link href="css/learn2.css" rel="stylesheet">
-      <link href="https://fonts.googleapis.com/css?family=Qwigley" rel="stylesheet">
+
       <style>
         body {
-          background-color: #FAFAFA;
-            color: #3d3d3d
+          background-color: #FFF;
+
         }
         .navbar{
           font-size: 15px;
@@ -52,32 +77,40 @@
             padding: 24px 15px;
             border-bottom: 3px solid #f4f4f4;
         }
+
         .nav-item:hover, .active { 
+            /*background-color: rgba(199, 196, 196, 0.1);*/
+            /*border-bottom: 3px solid rgb(90, 145, 247);*/
             border-bottom: 3px solid #2196F3;
         }
+
         footer {
           background: #FAFAFA;
         }
+
         .justify-space-between {
           justify-content: space-between;
         }
+
         .wrap {
           flex-wrap: wrap;
         }
-          .btn-primary {
-        border-radius: 8px; 
-        background-color: #2196F3; 
-        border-color: #2196F3;
-    }
-    .btn-primary:hover,
-    .btn-primary:active,
-    .btn-primary:visited,
-    .btn-primary:focus {
-        background-color: #0475CE !important;
-    }
+
+         /* media queries */
+        @media (max-width: 599px) { 
+            .navbar {
+                padding: 1em;
+            }
+            .login-con, .reset-title {
+			    width: 100% !important;
+		    }
+        }   
+	.headerLogo {
+	      width: 100% !important;
+		height: 30px !important;
+	      }
     </style>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" type="text/javascript">
-</script>
+
   </head>
 
   <body>
@@ -85,8 +118,9 @@
     
     <nav class="navbar navbar-expand-lg navbar-light"  >
       <a class="navbar-brand" href="../index.php">
-        <img src="img/logo.png" alt="HNG logo" class="img-fluid">
+        <img src="img/approved_HNG_logo.png" alt="HNG logo" class="img-fluid headerLogo">
       </a>
+
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -95,28 +129,30 @@
         
       
         <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
+            <li class="nav-item <?= $activeArray[0] ?>">
                 <a href="index.php" class="nav-link">Home</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item <?= $activeArray[1] ?>">
                 <a href="learn.php" class="nav-link">Learn</a>
             </li> 
-            <li class="nav-item">
+            <li class="nav-item <?= $activeArray[2] ?>">
                 <a href="listing.php" class="nav-link">Intern</a>
             </li> 
-            <li class="nav-item">
+            <li class="nav-item <?= $activeArray[3] ?>">
                 <a href="testimonies.php" class="nav-link">Testimonies</a>
             </li> 
-            <li class="nav-item">
+            <li class="nav-item <?= $activeArray[4] ?>">
                 <a href="sponsors.php" class="nav-link">Sponsors</a>
             </li> 
-            <li class="nav-item">
+            <li class="nav-item <?= $activeArray[5] ?>">
                 <a href="alumni.php" class="nav-link">Alumni</a>
             </li> 
-           <li class="nav-item">
+           <li class="nav-item <?= $activeArray[6] ?>">
                 <a href="partners.php" class="nav-link">Partners</a>
             </li> 
     </ul>
   </div>
          
     </nav>
+
+    
