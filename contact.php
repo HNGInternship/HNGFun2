f5506216261fccb8204fb676c4b480c791fcc1c3
Adokiye <38386612+Adokiye@users.noreply.github.com>
2018-05-01 18:59:00 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,60 +1,63 @@
 <?php
 include_once("header.php");
- function custom_styles() {
-     $styles = '<style>body{background:#e5e5e5!important;}</style>';
-     echo $styles;
- };
+function custom_styles() {
+    $styles = '<style>body{background:#e5e5e5!important;}</style>';
+    echo $styles;
+};
 ?>
 
- <div class="jumbotron contact bg-cover">
-     <div class="overlay"></div>
-      <div class="container contact">
-        <h1>Get In Touch</h1>
-        <p>Showing up is 80% percent of life</p>
+    <div class="jumbotron contact bg-cover">
+        <div class="overlay"></div>
+        <div class="container contact">
+            <h1>Get In Touch</h1>
+            <p>Showing up is 80% percent of life</p>
 
-      </div>
+        </div>
     </div>
 
-    <div class="container jumbotron contact-form">
+    <div class="container jumbotron contact-form" style="padding: 0">
         <div class="row">
             <div class="col col-sm-7 contact-form">
                 <div class="row">
-                    <h3>Send us a message</h3>
+                    <h3 style="text-align: center"> Send us a message</h3>
                     <span></span> <!-- for the envelope icon on the right -->
                 </div>
                 <form method="post">
-                    <div class="form-row">
+                    <span style="text-align: center">
+                      <div class="form-row">
                         <div class="form-group col-md-5">
                             <label>Your Name<label>
-                            <input type="text">
+                                    <input type="text">
                         </div>
 
                         <div class="form-group col-md-5">
                             <label>Email Address<label>
-                            <input type="text">
+                                    <input type="text">
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-5">
                             <label>Phone Number<label>
-                            <input type="text">
+                                    <input type="text">
                         </div>
 
                         <div class="form-group col-md-5">
                             <label>Subject<label>
-                            <input type="text">
+                                    <input type="text">
                         </div>
                     </div>
                     <div class="form-row">
                         <label>Message<label>
                     </div>
+                    </span>
                     <div class="form-row col-md-8">
+                      <span style="text-align: center">
                         <input type="text" placeholder="Type your message...">
-                    </div>
+                    </span></div>
                 </form>
             </div>
 
-            <div class="col col-sm-5 contact-info">
+            <div class="col col-sm-5 contact-info" style ="background: #2196F3;color: white">
                 <div class="row">
                     <h3>Contact Information</h3>
                 </div>
