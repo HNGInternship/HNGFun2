f879d5e17a34367a8aacc7305df453bb6a69fd7c
nellybaz10@gmail.com <35724948+nellybaz@users.noreply.github.com>
2018-05-01 13:38:19 +0200

    update

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ DO NOT MODIFY THIS FILE!!!
  */
 
 /* Instead of rediting this, just move your config.php a step outside the HNGFun folder */
-require 'config.example.php';
+require '../config.php';
 
 // try {
 //     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
