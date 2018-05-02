53ecb178bfdfa816841c57e3fa2721b45f990056
nellybaz10@gmail.com <35724948+nellybaz@users.noreply.github.com>
2018-05-02 15:59:53 +0200

    working on a dummy folder for reset password. Not touching any other file

--- check/query.php
+++ check/query.php
@@ -4,16 +4,11 @@ include('../../db.php');
 
 $query = "SELECT * FROM users";
 $result = mysqli_query($db, $query);
-if(mysqli_num_rows($result) > 0){
 $row = mysqli_fetch_array($result);
 while($row){
 	echo $row['first_name'];
 	echo $row['email'];
 }
-
-
-}
-
 
 else{
 	echo "nothing found";
