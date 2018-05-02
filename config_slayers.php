3bee9337d9e4d78d5dd3bc1ad721209c0183b551
Dennis W. Otugo <otugodennis@gmail.com>
2018-05-02 09:16:27 +0100

    Update config_slayers.php

--- config_slayers.php
+++ config_slayers.php
@@ -1,11 +1,8 @@
 <?php
 
 define ('DB_USER', "root");
-<<<<<<< HEAD
 define ('DB_PASSWORD', "29gE9t*dJ2#2f-BS");
-=======
 define ('DB_PASSWORD', "");
->>>>>>> 22705b18d1b44c2c5da6d06de97d0e2d23cb9cfc
 define ('DB_DATABASE', "slayers_db");
 define ('DB_HOST', "localhost");
 
