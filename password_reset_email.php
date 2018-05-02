35fa18c1a81fa8306dc4999eb2f86d5cbb6b2a38
Osumgba Chiamaka <31241128+osumgbachiamaka@users.noreply.github.com>
2018-05-02 21:41:07 +0100

    alignment issues corrected

--- password_reset_email.php
+++ password_reset_email.php
@@ -305,8 +305,8 @@
                   <tr>
                     <td align="center" style="font-size:0px;padding:10px 25px;padding-top:50;word-break:break-word;">
 
-                      <div style="font-family:Lato;font-size:32px;font-weight:bold;letter-spacing:30%;line-height:24px;text-align:center;color:#3D3D3D;margin-top:-35px;">
-                        Please Reset your Password
+                      <div style="font-family:Lato;font-size:32px;font-weight:bold;letter-spacing:30%;line-height:24px;text-align:center;color:#3D3D3D;margin-top:-40px;">
+                        <span>Please Reset your Password<span>
                       </div>
 
                     </td>
