93a2c4a67c713513fc886620619d1ef59d19b0a0
nedy udombat <nedyudombat@gmail.com>
2018-05-02 10:27:27 +0100

    removed one logo

--- header.php
+++ header.php
@@ -84,13 +84,9 @@
     <!-- Navigation -->
     
     <nav class="navbar navbar-expand-lg navbar-light"  >
-
+      
       <a class="navbar-brand" href="../index.php">
-        <img src="img/logo.png" alt="HNG logo" class="img-fluid">
-
-      <a class="navbar-brand" href="#">
         <img src="img/approved_HNG_logo.png" alt="HNG logo" width="128" height="52" class="img-fluid">
-
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
