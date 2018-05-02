9dd204753e9787ef3fb7254f6c5ba15dc3dcf8c6
Angela Bello <angelabisibello@gmail.com>
2018-05-02 12:45:54 +0200

    remove slayer team’s changes to db.php

--- db.php
+++ db.php
@@ -2,20 +2,9 @@
 /*
 DO NOT MODIFY THIS FILE!!!
  */
-require 'config_slayers.php';
-
-// try {
-//     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-  
-// } catch (PDOException $pe) {
-//     die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-// }
-
-
-
 /* Instead of rediting this, just move your config.php a step outside the HNGFun folder */
 require '../config.php';
- 
+
 try {
     $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
 
