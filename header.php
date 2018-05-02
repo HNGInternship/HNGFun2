a105c2e67dfe9ea21f80c0092d3a73494193e26e
Ogoluwa Ojewale <simeondgenius@gmail.com>
2018-05-02 22:26:53 +0100

    header.php

--- header.php
+++ header.php
@@ -42,49 +42,110 @@
       
 
       <style>
-        body {
-          background-color: #FAFAFA;
+        body{
+            background-color: #fafafa;
+            font-family: 'Lato', sans-serif;
         }
-        .navbar{
-          font-size: 15px;
-          font-weight: bold;
-          padding-right: 100px;
-          padding-left: 100px;
-          background-color: #FFFFFF;
+        #navbar{
+            font-size: 15px;
+            font-weight: bold;
         } 
-  
+        
         .nav-item{
             padding-right: 15px;
             padding-left: 15px;
         }
+
         .nav-item:hover { 
             background-color: rgba(199, 196, 196, 0.1);
             border-bottom: 3px solid rgb(90, 145, 247);
-
         }
 
-        footer {
-          background: #FFFFFF;
+
+
+        li.nav-item {
+            padding-bottom: 0px;
+        }
+
+        ul.navbar-nav {
+            height: auto !important;
         }
 
   <?php if (function_exists('custom_styles')) {
       custom_styles();
     }
     ?>
+
+    nav.navbar {
+        box-sizing: border-box !important;
+        padding: 0px 50px !important;
+        font-size: 15px;
+        font-weight: bold;
+        display: inline-block;
+        width: 100%;
+            padding: 10px 50px !important;
+    }
+
+    .navbar-logo {
+        width: auto !important;
+    }
+
+    .navbar-brand {
+    width: auto !important;
+    }
+
+    @media (min-width: 992px){
+    .navbar-expand-lg .navbar-nav .nav-link {
+        padding-right: .5rem;
+        padding-left: .5rem;
+        padding-top: 20px;
+        font-size: 15px !important;
+    }
+
+    ul.navbar-nav.collapse.ml-auto {
+        display: -webkit-inline-box;
+        height: 100% !important;
+        float: right;
+    }
+
+    nav.navbar {
+        padding: 0 16px 0 50px !important;
+        height: 100px;
+    }
+
+    .navbar-logo {
+        width: auto !important;
+        margin-top: 30px;
+    }
+    }
+
+    .navbar-toggler {
+    float: right;
+    }
+
+    .nav-item.active {
+        background-color: rgba(199, 196, 196, 0.1);
+        border-bottom: 3px solid rgb(90, 145, 247);
+    }
+
+    .navbar-fixed {
+      background: #f4f4f4 !important;
+    }
+
     </style>
+
 
   </head>
 
   <body>
     <!-- Navigation -->
-    <div id="navbar">
+    <div id="navbar-fixed" class="navbar-fixed">
         <nav class="navbar navbar-expand-lg navbar-light"  style="background-color: #f2f2f2;">
         <a class="navbar-brand" href="./index.php"><img src="./img/logo.png" alt="" class="navbar-logo"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-            
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav collapse ml-auto">
                 <li class="nav-item">
                     <a href="index.php" class="nav-link">HOME</a>
                 </li>
