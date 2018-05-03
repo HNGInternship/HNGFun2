1fe36bd593bbdf82b0c9d73017dbdc0dd6c9e310
saslamp <sadiqasg@gmail.com>
2018-05-03 12:57:36 +0100

    added resetpasswordemail.php

---
+++ resetpasswordemail.php
@@ -0,0 +1,59 @@
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- styles link -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+
+    <style type="text/css">
+    	body {    		
+    		width: 100%;
+	    	padding: 0;
+    		margin: 0;
+    		font-family: 'Lato';
+    		font-size: 20px;
+    	}
+    	h2 {
+    		font-weight: bolder;
+    	}
+    	section {
+    		margin: 4em auto auto;
+    	}
+    	p {
+    		margin: 0px;
+    	}
+    	.foot {
+    		margin-top: 1.5em;
+    	}
+    </style>
+
+</head>
+
+<body>
+	<main class="container text-center">
+		<div class="row">
+			<div class="col-md-12">
+              <img alt="HNG logo" width="150px" style="margin-top:1em;" src="https://res.cloudinary.com/damilare1957/image/upload/v1525088492/logo.png"/>
+			</div>
+		</div>
+
+		<section>
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Please Reset Your Password</h2>
+					<p>We heard that you lost your HNGInternship password. Sorry about that!</p>
+					<p>But don't worry! You can use the link to reset your password:</p>
+					<p>To get a new password reset link, visit <a href="http://revenge.hng.fun/resetpassword.php">here</a></p>
+				</div>
+			</div>
+
+			<div class="row foot">
+				<div class="col-md-12">
+					<p>Thanks,<br/>Your friends at HNG.</p>
+				</div>
+			</div>
+		</section>
+	</main>
+</body>
