813233d7efc4674ec7961cb744c8e0f48959a4b7
Chigozie <chygoz2@gmail.com>
2018-05-03 12:01:39 +0100

    Configuring smtp details

---
+++ smtp_credentials.php
@@ -0,0 +1,7 @@
+<?php
+    define('SMTP_USER', 'mail@gmail.com');
+    define('SMTP_PASSWORD', 'password');
+    define('SMTP_HOST', 'smtp.gmail.com');
+    define('SMTP_PORT', '587');
+    define('SMTP_PROTOCOL', 'tls');
+?>
