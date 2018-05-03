8e71e34797a6cdf3f4d8fadadeb30cf2e9559c19
olamide Faniyan <faniyanolamide@gmail.com>
2018-05-03 19:34:47 +0100

    past

--- pastProjects.php
+++ pastProjects.php
@@ -10,9 +10,33 @@ include_once("header.php");
 		font-size: 23px;
 		position: relative;
 		margin-bottom: 150px;
+		cursor: pointer;
 	}
-	p{
+	#p{
 		border-radius: 20px;
+		width: 300px; 
+		height: 200px;
+		position: absolute;
+		background-position: center;
+		background-size: cover;
+		-webkit-background-size: cover; 
+		margin: 0; 
+		padding: 0; 
+		padding-left: 200px;
+		text-align: center;
+	}
+
+	.col-lg-4  #p :hover{
+		border: 1px solid blue;
+	}
+	#dev:hover{
+		color: blue;
+	}
+	span{
+		font-size: 18px
+	}
+	#dev{
+		position: relative;padding-top: 15%;text-align: center; font-weight: bold;padding-right: 60px; color: white; font-size: 30px
 	}
 </style>
 
@@ -21,48 +45,47 @@ include_once("header.php");
 	<div class="text-center">
 		<h2 style="font-weight: bold; font-size: 40px;">Things Built</h2>
 		<span style="font-weight: bold; font-size: 10px;"><hr style="width: 100px; border: 1px solid"></span>
-		<p style="font-size: 17px; margin-bottom: 25px">HNG has been a life transforming journey for both new and experienced developer across the whole<br/> world expecially africa.
-		Check all project made by participants when grouped into teams.</p>
+		<div style="font-size: 17px; margin-bottom: 25px">HNG has been a life transforming journey for both new and experienced developer across the whole<br/> world expecially africa.
+		Check all project made by participants when grouped into teams.</div>
 	</div>
 	<div align="center">
 		<div class="row">
 			<div class="col-lg-4" style="position: relative;">
-				<p style="width: 300px; height: 200px; background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url(https://d.novoresume.com/images/doc/college-cv-template.png);position: absolute; background-position: center; opacity: ; background-size: cover; -webkit-background-size: cover; margin: 0; padding: 0; padding-left: 200px;"></p>
-				<div style="position: relative;padding-top: 15%;text-align: center; font-weight: bold;padding-right: 60px; color: white; font-size: 30px">Cv Designs<br/><span style="font-size: 18px">1700 Interns</span></div>
+				<p id="p" style="background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url(https://d.novoresume.com/images/doc/college-cv-template.png);"></p>
+				<div id="dev">Cv Designs<br/><span>1700 Interns</span></div>
 			</div>
 			<div class="col-lg-4">
-				<p style="width: 300px; height: 200px; text-align: center; padding-top: 25%; background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTttJw0TufJgVXy4WrDhbaPcZ31HrqL38eAMGuOHq-UFXNQETQl); background-position: center; background-size: cover; -webkit-background-size: cover; font-weight: bold; position: absolute;; margin: 0; padding: 0"></p><div style="position: relative; padding-top: 15%;text-align: center; font-weight: bold;padding-right: 60px;; color: white; font-size: 30px"> Transcribe app<br/><span style="font-size: 18px">200 Interns</span></div>
+				<p id="p" style="background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTttJw0TufJgVXy4WrDhbaPcZ31HrqL38eAMGuOHq-UFXNQETQl);"></p><div id="dev"> Transcriber app<br/><span>200 Interns</span></div>
 			</div>
 			<div class="col-lg-4">
-				<p style="margin: 0; padding: 0;width: 300px; height: 200px; text-align: center; padding-top: 25%; background-position: center; background-size: cover; -webkit-background-size: cover; background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFn-00QAOXZBeLhETx1HwDlHw5JHHyWDAyMBwEnlRGu3Ird9K9Eg); position: absolute;"></p><div style="position: relative;color: white; font-size: 30px;padding-top: 15%;text-align: center; font-weight: bold;padding-right: 60px;"> Ig Vendors<br/><span style="font-size: 18px">27 Interns</span></div>
+				<p  id="p" style="background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFn-00QAOXZBeLhETx1HwDlHw5JHHyWDAyMBwEnlRGu3Ird9K9Eg);"></p><div id="dev"> Ig Vendors<br/><span>27 Interns</span></div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-lg-4">
-				<p style=" margin: 0; padding: 0;width: 300px; height: 200px; text-align: center; padding-top: 25%; background-position: center; background-size: cover; -webkit-background-size: cover; background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2FIZQvP4PRirshrIqCK5XTd6W1zsExEefvT9m2xGtU9kNcbLlXA); position: absolute;"></p><div style="position: relative; font-size: 30px; color: white; text-align: center;padding-top: 15%;text-align: center; font-weight: bold;padding-right: 60px;"> Wireframe<br/><span style="font-size: 18px">10 Interns</span></div>
+				<p id="p" style="background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2FIZQvP4PRirshrIqCK5XTd6W1zsExEefvT9m2xGtU9kNcbLlXA);"></p><div id="dev"> Wireframe<br/><span>10 Interns</span></div>
 			</div>
 			<div class="col-lg-4">
-				<p style="margin: 0; padding: 0;width: 300px; height: 200px; text-align: center; padding-top: 25%; background-position: center; background-size: cover; -webkit-background-size: cover; background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUpu6fNbNOAeJpjSKJ9i9e9TdAVKxcBKaP7VwBmIhNDBxXi3WAqA); position: absolute;"></p><div style="position: relative; font-size: 30px; color: white; text-align: center;padding-top: 15%;text-align: center; font-weight: bold;padding-right: 60px;"> Power Pack<br/><span style="font-size: 18px">24 Interns</span></div>
+				<p id="p" style="background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUpu6fNbNOAeJpjSKJ9i9e9TdAVKxcBKaP7VwBmIhNDBxXi3WAqA);"></p><div id="dev"> Power Pack<br/><span>24 Interns</span></div>
 			</div>
 			<div class="col-lg-4">
-				<p style="margin: 0; padding: 0; width: 300px; height: 200px; text-align: center; padding-top: 25%; background-position: center; background-size: cover; -webkit-background-size: cover; background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLyA34KEHTfPTraZruJjKYeIO2kBTiPKQzC6TEBa1hP3LYKfMq); color: white; position: absolute;"></p><div style="padding-top: 15%;text-align: center; font-weight: bold;padding-right: 60px; position: relative; font-size: 30px; color: white;"> Banner design<br/><span style="font-size: 18px">400 Interns</span></div>
+				<p id="p" style="background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLyA34KEHTfPTraZruJjKYeIO2kBTiPKQzC6TEBa1hP3LYKfMq);"></p><div id="dev" "> Banner design<br/><span>400 Interns</span></div>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-lg-4">
-				<p style="margin: 0; padding: 0;margin-bottom: 60px;width: 300px; height: 200px; text-align: center; background-position: center; background-size: cover; -webkit-background-size: cover; background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXBCsMzKv9WxEAkD72wajAu2OVYdcWs8H9LVBKrousmA8lhq88BQ); position: absolute;"></p><div style="padding-top: 15%;text-align: center; font-weight: bold;padding-right: 60px; position: relative; color: white; font-size: 30px"> Fast Pay<br/><span style="font-size: 18px">68 Interns</span></div>
+				<p  id="p" style="background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXBCsMzKv9WxEAkD72wajAu2OVYdcWs8H9LVBKrousmA8lhq88BQ);"></p><div id="dev" "> Fast Pay<br/><span>68 Interns</span></div>
 			</div>
 			<div class="col-lg-4">
-				<p style="margin: 0; padding: 0;width: 300px;  height: 200px; text-align: center; background-position: center; background-size: cover; -webkit-background-size: cover; background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR86NHWKQO3eMVzheK4Hd0q7QS3piRtAkhcBaIwr3-nCXpk2BXc); position: absolute;"></p> <div style="position: relative; color: white; font-size: 30px; padding-top: 15%;text-align: center; font-weight: bold;padding-right: 60px;"> Profile Pages<br/><span style="font-size: 18px">4000 Interns</span></div>
+				<p  id="p"style="background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR86NHWKQO3eMVzheK4Hd0q7QS3piRtAkhcBaIwr3-nCXpk2BXc);"></p> <div id="dev"> Profile Pages<br/><span>4000 Interns</span></div>
 			</div>
 
 			<div class="col-lg-4">
-				<p id="emails" style="margin: 0; padding: 0;width: 300px; height: 200px; text-align: center; background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(http://res.cloudinary.com/pajimo/image/upload/v1525094886/download.png); background-position: center; background-size: cover; -webkit-background-size: cover;
- position: absolute;"></p><div style="font-size: 30px; color: white; position: relative;padding-top: 15%;text-align: center; font-weight: bold;padding-right: 60px;">Project Emails<br/><span style="font-size: 18px">70 Interns</span></div>
+				<p id="p" style="background:linear-gradient(0deg,rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(http://res.cloudinary.com/pajimo/image/upload/v1525094886/download.png); background-position: center; background-size: cover;"></p><div id="dev" ">Project Emails<br/><span>70 Interns</span></div>
 			</div>
 		</div>
 
-		<p style="margin-top: 20px; font-size: 18px">More projects coming up.. Check back later</p>
+		<div style="margin-top: 20px; font-size: 18px">More projects coming up.. Check back later</div>
 	</div>
 <style type="text/css">
 	/* Pagination links */
@@ -79,20 +102,18 @@ include_once("header.php");
     background-color: dodgerblue;
     color: white;
 }
-
+.pagination{
+	margin-top: 30px;
+	margin-left: 43%;
+}
 /* Add a grey background color on mouse-over */
 .pagination a:hover:not(.active) {background-color: #ddd;}
 </style>
-
-	<div class="text-center">
+	<div align="center">
 		<div class="pagination">
 		  <a href="#">&laquo;</a>
 		  <a class="active" href="#">1</a>
 		  <a href="#">2</a>
-		  <a href="#">3</a>
-		  <a href="#">4</a>
-		  <a href="#">5</a>
-		  <a href="#">6</a>
 		  <a href="#">&raquo;</a>
 		</div>
 	</div>
