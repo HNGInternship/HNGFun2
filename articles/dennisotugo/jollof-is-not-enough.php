5e4f2c2deaa9586191ac0c9597342ffeedb7163a
Dennis W. Otugo <otugodennis@gmail.com>
2018-05-04 02:58:18 +0100

    Update jollof-is-not-enough.php

--- articles/dennisotugo/jollof-is-not-enough.php
+++ articles/dennisotugo/jollof-is-not-enough.php
@@ -1,3 +1,36 @@
 <?php
 include_once("header.php");
 ?>
+<!DOCTYPE html>
+
+
+<html>
+    <head>
+        <title>
+            Breaking News: Cat does not give a damn
+        </title>
+        <style type="text/css">
+
+        </style>
+    </head>
+    <body>
+        <article>
+            <h1>Breaking News: Cat does not give a damn</h1>
+            <p><strong>Author:</strong> Bill Clinton, <strong>Published:</strong> Yesterday</p>
+
+            <div>
+                <img src="http://peoplecancode-public.s3.amazonaws.com/cat.jpg">
+                <span>Image source: The WEB</span>
+            </div>
+
+            <p>
+                This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text to low around it. I even decided to copy the previous paragraph. This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text to low around it.
+            </p>
+            <p>This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text fo low around it.</p>
+
+        </article>
+    </body>
+</html>
+    <?php
+include_once("footer.php");
+?>
