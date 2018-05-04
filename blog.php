8b8d0f035ba023660f87baad878d35158fa3d189
Dennis W. Otugo <otugodennis@gmail.com>
2018-05-04 23:52:31 +0100

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html>
+<?php 
+	require 'db.php';
+	require_once( ROOT_PATH . '/blog/public_functions.php') ?>
+<?php $posts = getPublishedPosts(); ?>
 <?php
 include_once("header.php");
 ?>
@@ -14,6 +18,20 @@ include_once("header.php");
 			<h2 class="content-title">Recent Articles</h2>
 			<hr>
 			<!-- more content still to come here ... -->
+			<?php foreach ($posts as $post): ?>
+	<div class="post" style="margin-left: 0px;">
+		<img src="<?php echo BASE_URL . '/images/' . $post['image']; ?>" class="post_image" alt="">
+		<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
+			<div class="post_info">
+				<h3><?php echo $post['title'] ?></h3>
+				<div class="info">
+					<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
+					<span class="read_more">Read more...</span>
+				</div>
+			</div>
+		</a>
+	</div>
+<?php endforeach ?>
 		</div>
 		<!-- // Page content -->
 
