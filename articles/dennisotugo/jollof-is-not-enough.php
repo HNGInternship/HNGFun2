aceab4260a1b5d6fc6d0ff00ede8efb64e2add10
Dennis W. Otugo <otugodennis@gmail.com>
2018-05-04 04:28:44 +0100

    Update jollof-is-not-enough.php

--- articles/dennisotugo/jollof-is-not-enough.php
+++ articles/dennisotugo/jollof-is-not-enough.php
@@ -10,19 +10,48 @@ include_once("../../header.php");
             Breaking News: Cat does not give a damn
         </title>
         <style type="text/css">
-
+.featured-image {
+    position: absolute;
+    width: 100%
+        
+            }
+            .h1, h1 {
+    font-family: Lato;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    font-size: 34px;
+    text-align: center;
+    color: #3D3D3D;
+    /* font-size: 2.5rem; */
+}
+            p {
+    font-family: Lato;
+    font-style: normal;
+    font-weight: normal;
+    line-height: 44px;
+    font-size: 14px;
+    color: #000000;
+    /* margin-top: 0; */
+    /* margin-bottom: 1rem; */
+}
+            article {
+                border: 0.75px solid #3D3D3D;
+transform: matrix(1, 0, 0, 1, 0, 0);
+            }
         </style>
     </head>
     <body>
         <article>
-            <h1>Breaking News: Cat does not give a damn</h1>
-            <p><strong>Author:</strong> Bill Clinton, <strong>Published:</strong> Yesterday</p>
+            
 
-            <div>
-                <img src="http://peoplecancode-public.s3.amazonaws.com/cat.jpg">
+
+            <div class="featured-image">
+                <img src="/Rectangle%203-2.png">
                 <span>Image source: The WEB</span>
             </div>
-
+            <h1>Breaking News: Cat does not give a damn</h1>
+                        <p><strong>Author:</strong> Bill Clinton, <strong>Published:</strong> Yesterday</p>
             <p>
                 This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text to low around it. I even decided to copy the previous paragraph. This paragraph will include the photo of a random cat that the author of this tutorial has found online. You can replace this text with any random text. The sole purpose of this copy is to fill the space and make sure that the image to the left has enough text to low around it.
             </p>
