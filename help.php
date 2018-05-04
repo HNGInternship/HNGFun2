024e771b7431ff1428a2389bf054d0f736dcdd5d
Gwinyai <gtchax@gmail.com>
2018-05-04 22:05:09 +0200

    added toggle side bar to help page

--- help.php
+++ help.php
@@ -1,252 +1,427 @@
 <?php
 include_once("header.php");
 ?>
-
 <style>
-    body {
-        /* background-color: firebrick; */
-        font-family: 'Fruktur';
-        background: #ffffff !important;
-    }
 
-    .whole-background {
-        height: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+/* General styles */
+main {
+    position: relative;
+}
 
-    .container-fluid {
-        padding: 20px;
-        color: #fff;
-    }
+header {
 
-    .header-font-one {
-        font-size: large;
-        font-family: "serif";
-        color: white;
-    }
+    padding-bottom: 0;
+}
 
-    .header-font-one h2{
-        padding-top: 38px;
-        color: #ffffff;
-    }
+nav.navbar.navbar-expand-lg.navbar-light {
+    position: fixed;
+    left: 0;
+    right: 0;
+    z-index: 999;
 
-    .form-control {
+}
 
-        border: 1px solid #48BBFC !important;
-        color: #48BBFC;
-        text-align: center;
-        margin: 0 auto;
-        padding: .9rem 0;
-        font-size: 16px;
-    }
+.hero {
+    /* max-width: 640px; */
+    margin: 0 auto;
+  
+}
 
 
-    .search-container > form {
-        margin-top: 1em;
-        margin-bottom: 3em;
-    }
 
-    .header-font-two {
-        font-size: small;
-        font-family: "Apple Color Emoji";
-        text-align: center;
+.hero input[type="text"]{
+    padding: 10px;
+    padding-left: 10px;
+    text-align: left;
+    border: 1px solid #3d3d3d;
+ 
+    
+}
 
-    }
 
-    .search-container {
-        height: 165px;
-        width: 100%;
-        justify-content: center;
-    }
+.hero input[type="submit"] {
+    border-radius: 5px;
+    padding: 11.9px 25px;
+    background: #2196f3;
+    color: #fff;
+}
 
+
+
+  
     input[type="text"]#help-search::-webkit-input-placeholder { /* Chrome */
-        color: #48BBFC !important;
+        color: #3d3d3d !important;
     }
     input[type="text"]#help-search:-ms-input-placeholder { /* IE 10+ */
-        color: #48BBFC !important;
+        color: #3d3d3d !important;
     }
     input[type="text"]#help-search::-moz-placeholder { /* Firefox 19+ */
-        color: #48BBFC !important;
+        color: #3d3d3d !important;
         opacity: 1;
     }
     input[type="text"]#help-search:-moz-placeholder { /* Firefox 4 - 18 */
-        color: #48BBFC !important;
+        color: #3d3d3d !important;
         opacity: 1;
     }
 
-    #help-search{
-        width: 47%;
-    }
 
-    .header-container {
-        height: 200px;
-        width: 100%;
-        background-color: #3fb3fa;
-        justify-content: center;
-    }
+/* Profile styles */
+.sidebar {
+    position: fixed;
+    z-index: 99;
+    background: #fff;
+    top: 99.5px;
+    left: 0;
+    bottom: 0;
+    width: 230px;
+    -webkit-transform: translateX(-100%);
+    -moz-transform: translateX(-100%);
+    -ms-transform: translateX(-100%);
+    -o-transform: translateX(-100%);
+    transform: translateX(-100%);
+    -webkit-box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 0px 4px 0px rgba(0, 0, 0, 0.3);
+    -webkit-transition: all 0.3s cubic-bezier(0.86, 0, 0.07, 1);
+    -moz-transition: all 0.3s cubic-bezier(0.86, 0, 0.07, 1);
+    transition: all 0.3s cubic-bezier(0.86, 0, 0.07, 1);
+  
+  
+}
 
-    .faq-container {
-        position: relative;
-        background: #2196F3;
-        padding-left: 2.7em;
-        padding-right: 2em;
-        margin-bottom: 2em;
-    }
+.user-image {
+    height: 150px;
+    width: 150px;
+    border-radius: 50%;
+    text-align: center;
+    background: #5f5f5f;
+    z-index: 100;
+    margin: 2.4em 0 0.7em 1.2em;
+}
 
-    .faq-container .col.text-left > h2 {
-        color: #ffffff;
-        padding-left: 18px;
-    }
+.profile h5 {
+    color: #2196f3;
+    font-weight: 500;
+    font-size: 22px;
+    margin-bottom: 1.8em;
+    margin-top: 1.2em;
+    text-align: center;
+}
 
-    .card {
-        background: #2196F3;
-        border: none;
-        border-radius: 0;
-
-    }
-
-    .card-header{
-        background-color: #2196F3;
-        color: #ffffff;
-        border: none;
-    }
-
-    .card-header button {
-        color: #ffffff;
-        text-transform: none;
-    }
-
-    .btn {
-        padding: 0;
-    }
-
-    i.fa.fa-chevron-down {
-        color: #ffffff !important;
-        padding-left: 24px;
-    }
-
-
-    .contact-container > h4{
-        margin-bottom: 1em;
-        color: #2196F3;
-        text-align: center;
-
-    }
-
-    .closing {
-        display: none;
-        transition: transform 80000ms ease-in-out 10ms;
-    }
-
-    .collapse.closing {
-        translateY: -10%
- transition: transform 80000ms ease-in-out 10ms;
-    }
+aside.sidebar.visible {
+  -webkit-transform: translateX(0);
+  -moz-transform: translateX(0);
+  -ms-transform: translateX(0);
+  -o-transform: translateX(0);
+  transform: translateX(0);
+}
 
 
-    .faq-container img {
 
-        display: inline-block;
-         max-width: 165px;
+nav {
+    display: flex;
+    flex-direction: column;
+    padding: 0;
+    margin: 0;
+    align-items: center;
+}
 
-    }
+.profile-nav a {
+    border: 1px solid #2196f3;
+    width: 188px;
+    margin-bottom: 1.8em;
+    padding: 9px 10px;
+    border-radius: 5px;
+    color: #2196f3;
+    text-transform: uppercase;
+}
 
-    button.btn.btn-link {
-        font-weight: 300;
-        font-size: .9em;
+a.btn.help-btn-blue {
+    color: #ffffff;
+    background: #2196f3;
+}
 
-    }
+.help-blue-btn {
+    color: #fff;
+    background: #2196f3;
+}
 
-    .form-wrapper {
-       display: block;
-        max-width: 30em;
-        padding: 4em 1.8em 4.2em;
+.help-white-btn {
+    background: #fff;
+    color: #2196f3;
+    border: 1px solid #2196f3;
+
+}
+
+/*FAQ styles */
+
+.jumbotron {
+  
+    background: #f4f4f4;
+    text-align: center;
+    color: #3D3D3D;
+    border: 0;
+    border-radius: 0;
+    margin: 0;
+    margin-bottom: 0;
+    padding-top:  150px;
+     /* position: fixed; */
+}
+
+
+
+.faq-wrapper {
+    border: 1px solid #4f4f4f;
+    margin: 3em 0 3em;
+    padding: 0.8em;
+    border-top-right-radius: 5px;
+    border-top-left-radius: 5px;
+    text-align: right;
+
+}
+
+/* FORM styles */
+
+ .form-wrapper {
+      
+        display: block;
+        max-width: 50em;
+        padding: 3em 1.8em 4.2em;
         margin: 0 auto;
-        border: 1px solid #48BBFC;
+        border: 1px solid #3d3d3d;
         border-radius: .25em;
-
+        
     }
 
-    .form-wrapper  label {
-        text-align: left;
-         color: #5F5F5F;
-         font-size: 0.9em;
-    }
+.form-wrapper h5 {
+   
+    text-align: center;
+}
 
-    .form-wrapper input[type="email"] {
-        margin-bottom: 2em;
-    }
+.answers {
+    margin-bottom: 2em;
+   
+}
 
-    .contact-container button.btn.btn-lg {
-        margin: 0;
-        display: inline-block;
-        margin-top: 1.2em;
-        color: #ffffff;
-        background: #2196F3;
-        padding: .35em 1.75em;
-        border-radius: .25em;
-        font-weight: 300;
-        text-align: right !important;
-    }
+#tab2, #tab1 {
+    margin: 0 !important;
+    padding: 0 !important;
+}
+
+.faq-wrapper h5 {
+    text-transform: uppercase;
+    padding-right: 15px;
+     text-align: center;
+}
+
+.faq-h5 {
+    text-align: center;
+    font-weight: 200;
+    padding-bottom: .7em;
+}
+
+.form-wrapper textarea, .form-wrapper input[type="email"] {
+    max-width: 35em;
+    margin-left:auto;
+    margin-right: auto;
+    padding-top: 1em;
+    padding-bottom: 1em;
+}
+
+.form-wrapper input[type="submit"] {
+    text-transform: uppercase;
+    color: #fff;
+    background: #2196f3;
+    padding: 15px 45px;
+    border-radius: .25em;
+    border: none;
+    font-size: 12px;
+    float: right;
+}
 
 
-      #help-send {
-
-       text-align: right !important;
-       float: right;
-
-
-  }
-
-  button#help-send:after {
-
+.form-wrapper input[type="submit"]:after {
+     
       display: table;
       clear: both;
-
+   
   }
 
-    @media (max-width: 670px) {
-        .faq-container img {
-            display: none;
-        }
-    }
+.form-control {
+    border: 1px solid #5f5f5f;
+    margin-bottom: 2em;
+}
 
-    @media (min-width: 740px) {
-        .faq-container img {
-            padding-left: 30px;
-       max-width: 230px;
-        }
-    }
+.tabs {
+    margin-right: 0;
+    padding-right: 0;
+}
 
+.tabs a {
+    display: block;
+    padding: 15px 27px 10px 15px;
+    margin-left: 16px;
+    margin-bottom: 2.8em;
+    text-align: center;
+
+}
+
+.btn-q {
+    background: #f2f2f2;
+    color: #3d3d3d;
+    text-align: left;
+    padding-right: 2em
+    border: 0;
+}
+
+
+
+.nav-btn {
+    color: red;
+    z-index: 1001;
+    position: fixed;
+    top: 100px;
+    left: 0;
+    cursor: pointer;
+    background: #ffffff;
+    padding: 4px 8px;
+}
+
+.card {
+    border: none;
+    background: #f2f2f2;
+    margin-right: 5px;
+    margin-left: 5px;
+}
+
+.card-header {
+    background: #f2f2f2;
+}
+
+.card-body {
+    height: 330px;
+    font-size: 16px;
+}
+
+    @media (min-width: 1002px) {
+        .form-wrapper input[type="submit"] {
+            
+            display: inline-block;
+            position: relative;
+            left: -142px;
+        
+        }
+        .hero {
+            max-width: 640px;
+        }
+
+        .container {
+            padding-left: 200px;
+        }
+
+        .jumbotron {
+            margin-left: 200px;
+        }
+        .faq-wrapper {
+           
+            margin: 5em 0 3em;
+        }
+        .card-header h5.mb-0 i.fa.fa-chevron-down {
+            margin-left: 2em;
+  
+        }
+        .answers {
+            margin-bottom: 2em;
+   
+        }
+          .menu-toggle {
+            display:none;
+        }
+        .sidebar {
+            -webkit-transform: translateX(0);
+            -moz-transform: translateX(0);
+            -ms-transform: translateX(0);
+            -o-transform: translateX(0);
+            transform: translateX(0);
+        }
+
+    }
+        i.fa.fa-bars {
+            color: #3d3d3d !important;
+        }
+        .hero input[type="text"]{
+            padding: 10px;
+            padding-left: 50px;
+            margin-left: 10px;
+            border: 1px solid #3d3d3d;
+        }
+      
 </style>
-<main>
-    <div>
 
-        <section class="header-container text-center">
-            <div class="header-font-one">
-                <h2>Help Lab</h2>
-                <p>What Can we help you with</p>
-            </div>
-        </section>
 
+<header class="jumbotron">
+<div class="hero">
+ <h1>Help Center</h1>
+    <p>What Can we help you with ?</p>
+    <div class="form-row">
+        <div class="col-sm-9">
+            <input type="text" class="form-control" placeholder="Have a question? Ask or enter a search term here">
+        </div>
+        <div class="col">
+            <input type="submit" value="search" class="btn ">
+        </div>
     </div>
-    <section class="container-fluid search-container">
-          <form action="">
-            <input id="help-search" type="text" placeholder="...Search for a problem" class="form-control">
-        </form>
-    </section>
-    <section class="container-fluid faq-container">
-        <div class="row">
-              <div class="col text-left">
-                <h2 class="text-left">FAQ</h2>
-                <div id="accordion">
+</div>
+</header>  
+<main class="">
+<span class="menu-toggle">
+				<span class="nav-btn rounded-right"><i class="fa fa-bars"></i></span>
+			</span>
+    <aside class="col sidebar">
+  
+        <article class="profile">
+            <!-- <img src="" alt=""> -->
+            <div class="user-image"></div>
+            <h5>rock_zion</h5>
+        </article>
+   
+        <nav class="profile-nav">
+            <a href="#" class="btn" >Profile</a>
+            <a href="#" class="btn" >Trade</a>
+            <a href="#" class="btn" >Accolade Balance</a>
+            <a href="#" class="btn help-btn-blue" >Help &amp; Feedback</a>
+        </nav>
+    </aside>
+
+    <section class="container">
+        <div class="faq-wrapper">
+            <h5>faq's</h5>
+            <section>
+                <div class="key">
+                    <a href=""></a>
+                    <a href=""></a>
+                </div>
+                <div class="faqs">
+                    <div class="accordion">
+                    
+                    </div>
+                </div>
+            </section>
+        </div>
+        <div class="answers">
+            <div class="row">
+                <div class="col-md-3" id="tab1">
+                    <div class="tabs">
+                        <a href="#" class="help-blue-btn rounded-left">Private Key</a>
+                        <a href="#" class="help-white-btn rounded-left">Accolades</a>
+                    </div>
+                </div>
+                <div class="col" id="tab2">
+                    <div id="accordion">
                   <div class="card">
                     <div class="card-header" id="headingOne">
                       <h5 class="mb-0">
-                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                          Would I be awarded a certificate after this internship?
+                        <button class="btn-q btn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                          What is a private key?
                          <i class="fa fa-chevron-down" aria-hidden="true"></i>
                         </button>
                       </h5>
@@ -264,7 +439,7 @@ include_once("header.php");
                   <div class="card">
                     <div class="card-header" id="headingTwo">
                       <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        <button class="btn-q btn collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                           Is this internship program open for only Nigerians?
                          <i class="fa fa-chevron-down" aria-hidden="true"></i>
                         </button>
@@ -272,7 +447,7 @@ include_once("header.php");
                     </div>
                     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                       <div class="card-body">
-                        No, not at all. The HNG internship program is open for residents of African countries. We believe by creating an equal opportunity, we not only create human resource value by equipping our interns with the proper technical skills, we also make them employable thereby creating an adequately skilled, qualified workforce in Africa.
+                        No, no at all. The HNG internship program is open for residents of African countries. We believe by creating an equal opportunity, we not only create human resource value by equipping our interns with the proper technical skills, we also make them employable thereby creating an adequately skilled, qualified workforce in Africa.
 
                       </div>
                     </div>
@@ -280,7 +455,7 @@ include_once("header.php");
                   <div class="card">
                     <div class="card-header" id="headingThree">
                       <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <button class="btn-q btn collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                           Do I need any prior programming experience to join?<i class="fa fa-chevron-down" aria-hidden="true"></i>
                         </button>
                       </h5>
@@ -294,7 +469,7 @@ include_once("header.php");
                   <div class="card">
                     <div class="card-header" id="headingfour">
                       <h5 class="mb-0">
-                        <button  class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapsefour" aria-expanded="false" aria-controls="collapsefour">
+                        <button  class="btn-q btn collapsed" data-toggle="collapse" data-target="#collapsefour" aria-expanded="false" aria-controls="collapsefour">
                           What are the basic requirements for this program?
                           <i class="fa fa-chevron-down" aria-hidden="true"></i>
                         </button>
@@ -311,26 +486,18 @@ include_once("header.php");
                     </div>
                   </div>
                 </div>
-            </div>
-            <div class="col text-right">
-                <img src="img/conversation(1).png" alt="speech bubbles">
+                </div>
             </div>
         </div>
-    </section>
-    <section class="container-fluid  contact-container">
-        <h4>We would like to hear from you</h4>
+        
+        <h5 class="faq-h5">We would like to here from you</h5>
         <div class="form-wrapper">
-            <form action="">
-                <label class="text-left" for="email">Email address</label>
-                <input type="email" name="email" class="form-control">
-
-                <label class="text-left" for="email">Description</label>
-                <textarea name="description" id="" cols="30" rows="10" class="form-control"></textarea>
-                <button id="help-send" class="btn btn-lg">send</button>
-
+            <form action="POST">
+                <input type="email" name="" id="" class="form-control" placeholder="Email Adress">
+                <textarea name="" id="" cols="20" rows="5" class="form-control" placeholder="Message"></textarea>
+                <input type="submit" value="Send" class=" help-btn-blue"> 
             </form>
         </div>
-
     </section>
 
 </main>
@@ -341,15 +508,40 @@ include_once("header.php");
 include_once("footer.php");
 function custom_scripts() {
 
-       $script = "<script>
+       $script = "<script> 
        (function(){
-           var check = 0;
-          //  $('.panel-title').click(function(){
+      
+		var toggle = jQuery('.menu-toggle'),
+			mainNav = jQuery('.sidebar');
 
-              //  $('.collapse').collapse('toggle');
+		toggle.on('click', function () {
+			toggle.toggleClass('active');
+			mainNav.toggleClass('visible');
 
-            })
+			return false;
+		});
 
+		jQuery(document).on('click', function () {
+			mainNav.removeClass('visible');
+			toggle.removeClass('active');
+		});
+
+		mainNav.on('click', function (e) {
+			e.stopPropagation();
+		});
+
+		jQuery('header nav').css({
+			'top': (jQuery('header').outerHeight())
+		});
+
+		jQuery(window).on('resize', function () {
+			jQuery('header nav').css({
+				'top': (jQuery('header').outerHeight())
+			});
+		});
+
+
+   
        })();
         </script>";
 
