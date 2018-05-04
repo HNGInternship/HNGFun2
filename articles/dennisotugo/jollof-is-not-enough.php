3c8be079f8cb7d6bd53de4328e7b05aa7fb027a0
Dennis W. Otugo <otugodennis@gmail.com>
2018-05-04 05:38:09 +0100

    Update jollof-is-not-enough.php

--- articles/dennisotugo/jollof-is-not-enough.php
+++ articles/dennisotugo/jollof-is-not-enough.php
@@ -24,13 +24,10 @@ $activeArray[$fileIndex]="active";
 
     <title>HNG FUN</title>
 
-    <!-- Bootstrap core CSS -->
-      <link href="../../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
 
       <!-- Custom fonts for this template -->
   <!-- Custom fonts for this template -->
-    <link href="../../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
@@ -105,7 +102,7 @@ $activeArray[$fileIndex]="active";
     font-family: Lato;
     font-style: normal;
     font-weight: normal;
-    line-height: 30px;
+    line-height: normal;
     font-size: 14px;
     color: #000000;
     /* margin-top: 0; */
@@ -283,9 +280,7 @@ padding: 20px;
    </div>
 </footer>
 
-    <!-- Bootstrap core JavaScript -->
-   <!-- <script src="vendor/jquery/jquery.min.js"></script>-->
-    <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 
     <!-- Custom scripts for this template -->
     <script src="../../js/hng.min.js"></script>
