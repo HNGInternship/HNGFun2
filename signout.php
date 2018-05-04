b0bcf4855402f489f47f53bd93104d49c99a7e6d
OvundahKing <ovundahnyenke@gmail.com>
2018-05-04 23:33:18 +0100

    Update signout.php

--- signout.php
+++ signout.php
@@ -26,7 +26,7 @@ include_once("header.php");
     
     
     footer {
-        background-color: blue !important;
+        opacity: 0 !important;
     }
     
     @media only screen and (max-width: 1000px){
