972f0957e566a1b3f9e56803f65c9f871be8eb13
Michael Ajetunmobi <mikoloxtra@gmail.com>
2018-05-04 16:46:21 +0100

    db fix

--- login.php
+++ login.php
@@ -3,8 +3,8 @@ ob_start();
 session_start();
 
 define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
-define ('DB_DATABASE', "dbtest");
+define ('DB_PASSWORD', "29gE9t*dJ2#2f-BS");
+define ('DB_DATABASE', "hng_fun");
 define ('DB_HOST', "localhost");
 $servername = DB_HOST;
 $username = DB_USER;
