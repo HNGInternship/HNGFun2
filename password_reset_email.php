d15402cd5f2cfc95c1ca2a7a2c34eea4771fe2b7
Samuel <wekesamuel@yahoo.com>
2018-05-04 12:45:14 +0100

    Password

--- password_reset_email.php
+++ password_reset_email.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title> Password Reset Email</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/password_reset_email.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/password_reset.css" />
 </head>
 
 <body>
