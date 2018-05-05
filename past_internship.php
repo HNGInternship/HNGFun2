4dd4066d42c4529082788a517cd2e4d5ffcd5cc9
Somiari Lucky <somiarilucky@gmail.com>
2018-05-05 16:52:10 +0100

    Update past internship page

--- past_internship.php
+++ past_internship.php
@@ -5,6 +5,8 @@ include_once("header.php");
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 
   <!-- styles link -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
@@ -14,6 +16,11 @@ include_once("header.php");
   <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
   <style>
+    
+    body { 
+      font-family: lato, sans-serif;
+    }
+    
     main {
       margin-top: 50px;
     }
