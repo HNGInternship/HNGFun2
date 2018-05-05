5015bd116b79bd1f027f627c0529cc16e37bd791
Onwuorah Okechukwu Benjamin <@bonwuorah@gmail.com>
2018-05-05 18:05:10 +0100

    edited header to add favicon

--- header.php
+++ header.php
@@ -129,7 +129,6 @@ $activeArray[$fileIndex]="active";
 
       <a class="navbar-brand" href="../learn">
         <img src="img/logo.png" alt="HNG logo" class="img-fluid">
-
         </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
