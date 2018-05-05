946ac28bfbae08d4b1887e9cbe6511be8b62881c
bogadeji <bogadeji@gmail.com>
2018-05-05 17:57:36 +0100

    notifications.php

---
+++ notifications.php
@@ -0,0 +1,18 @@
+<?php
+	include('header.php');
+?>
+<style type="text/css">
+	p {
+		font-size: 45px;
+		font-family: lato;
+		margin-top: 150px;
+	}
+</style>
+<div class="col text-center">
+	<p class="text-center">Message Sent Successfully</p>
+	<img src="img/notification.png">
+</div>
+
+<?php
+	include('footer.php');
+?>
