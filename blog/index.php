152dae33faa1a2e81ff39c58dd8651b95ef4c3ff
Dennis Otugo <otugodennis@gmail.com>
2018-05-05 03:02:25 +0100

    //

---
+++ blog/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require("../db.php");
+$pageDetails = getPageDetailsByName($currentPage);
+include("../header.php");
+?>
+<div class="row main-row">
+    <div class="8u">
+        <section class="left-content">
+            <h2><?php echo stripslashes($pageDetails["page_title"]); ?></h2>
+            <?php echo stripslashes($pageDetails["page_desc"]); ?>
+        </section>
+    
+    </div>
+
+</div>
+<?php
+include("../footer.php");
+?>
---
+++ blog/manage-site/index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * @author Shahrukh Khan
+ * @website http://www.thesoftwareguy.in
+ * @facebook https://www.facebook.com/Thesoftwareguy7
+ * @twitter https://twitter.com/thesoftwareguy7
+ * @googleplus https://plus.google.com/+thesoftwareguyIn
+ */
+header("Location:home.php");
+?>
