11c5e13d1afabc4b743ade9d33d9c6520c1b28d4
Dennis W. Otugo <otugodennis@gmail.com>
2018-05-05 01:06:11 +0100

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,43 +1,20 @@
-<!DOCTYPE html>
-<html>
-<?php require( ROOT_PATH . 'db.php'); ?>
-<?php require_once( ROOT_PATH . '/blog/public_functions.php'); ?>
-<?php $posts = getPublishedPosts(); ?>
 <?php
-include_once("header.php");
+
+require("blog/config.php");
+$pageDetails = getPageDetailsByName($currentPage);
+
+include("header.php");
 ?>
-<body>
-	<!-- Styling -->
-	<link rel="stylesheet" href="/css/blog_styling.css">
-	<!-- container -->
-	<div class="container">
+<div class="row main-row">
+    <div class="9u">
+        <section class="left-content">
+            <h2><?php echo stripslashes($pageDetails["page_title"]); ?></h2>
+            <?php echo stripslashes($pageDetails["page_desc"]); ?>
+        </section>
+    
+    </div>
 
-		<!-- Page content -->
-		<div class="content">
-			<h2 class="content-title">Recent Articles</h2>
-			<hr>
-			<!-- more content still to come here ... -->
-			<?php foreach ($posts as $post): ?>
-	<div class="post" style="margin-left: 0px;">
-		<img src="<?php echo BASE_URL . '/images/' . $post['image']; ?>" class="post_image" alt="">
-		<a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
-			<div class="post_info">
-				<h3><?php echo $post['title'] ?></h3>
-				<div class="info">
-					<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
-					<span class="read_more">Read more...</span>
-				</div>
-			</div>
-		</a>
-	</div>
-<?php endforeach ?>
-		</div>
-		<!-- // Page content -->
-
+</div>
 <?php
-include_once("footer.php");
+include("footer.php");
 ?>
-	</div>
-	<!-- // container -->
-</body>
-</html>
