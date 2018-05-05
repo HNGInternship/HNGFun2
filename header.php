2ca6c6dcaa65ccdc526a95f4e3d0475cb81ba3e8
Onwuorah Okechukwu Benjamin <@bonwuorah@gmail.com>
2018-05-05 19:11:15 +0100

    edited header to add favicon

--- header.php
+++ header.php
@@ -1,15 +1,25 @@
 <?php
 if(!isset($_SESSION)) { session_start(); }
+
 // for choosing active page on nav bar
+
 $fileName=basename($_SERVER['PHP_SELF']);
+
 $files = array('index.php','learn.php','listing.php','testimonies.php','sponsors.php','alumni.php','partners.php');
 $activeArray = array('','','','','','', '');
+
 $fileIndex=array_search($fileName,$files);
+
+
 // if page is unknown, dont mark any nav item
+
 if($fileIndex!==FALSE){
+
 $activeArray[$fileIndex]="active";
 }
+
 /////////////////////////////////////////////////////////
+
 ?>
 
 <!DOCTYPE html>
@@ -24,14 +34,16 @@ $activeArray[$fileIndex]="active";
 
     <title>HNG FUN</title>
 
+    <link rel="icon" href="https://res.cloudinary.com/nedy123/image/upload/v1525534693/Approved_HNG_Logo_xswasd.png" type="image/gif" sizes="16x16">
     <!-- Bootstrap core CSS -->
       <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      
 
 
       <!-- Custom fonts for this template -->
   <!-- Custom fonts for this template -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
@@ -46,9 +58,9 @@ $activeArray[$fileIndex]="active";
       <link href="css/style.css" rel="stylesheet">
       <link href="css/custom.css" rel="stylesheet">
      <!-- <link href="css/learn.css" rel="stylesheet"> -->
-<!--	  <link href="css/carousel.css" rel="stylesheet">-->
+<!--    <link href="css/carousel.css" rel="stylesheet">-->
       <link href="css/landing-page.min.css" rel="stylesheet">
-      <link href="css/learn2.css" rel="stylesheet">
+      
       <link href="https://fonts.googleapis.com/css?family=Qwigley" rel="stylesheet">
       <style>
         body {
@@ -61,12 +73,18 @@ $activeArray[$fileIndex]="active";
           background-color: #F4F4F4;
           padding: 0 10em;
         }
+
         .nav-item{
             padding: 24px 15px;
             border-bottom: 3px solid #f4f4f4;
         }
         .nav-item:hover, .active {
             border-bottom: 3px solid #2196F3;
+        }
+        /* horizontal line learn page */
+        hr.under-line {
+            width: 10%;
+            border-top: 3px solid #000;
         }
         .justify-space-between {
           justify-content: space-between;
@@ -85,15 +103,19 @@ $activeArray[$fileIndex]="active";
     .btn-primary:focus {
         background-color: #0475CE !important;
     }
+
+
       /*for footer*/
     .contact-icon{
       margin: 0px !important;
       padding: 0% 2%;
     }
+
     footer{
       background: #FAFAFA !important;
       color: #3D3D3D;
     }
+
     </style>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js" type="text/javascript">
 </script>
@@ -104,9 +126,10 @@ $activeArray[$fileIndex]="active";
 
     <nav class="navbar navbar-expand-lg navbar-light"  >
 
-      <a class="navbar-brand" href="../index.php">
-        <img src="img/approved_HNG_logo.png" alt="HNG logo" width="128" height="52" class="img-fluid">
-      </a>
+      <a class="navbar-brand" href="../learn">
+        <img src="img/logo.png" alt="HNG logo" class="img-fluid">
+
+        </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -122,7 +145,7 @@ $activeArray[$fileIndex]="active";
                 <a href="learn.php" class="nav-link">Learn</a>
             </li>
             <li class="nav-item <?= $activeArray[2] ?>">
-                <a href="listing.php" class="nav-link">Intern</a>
+                <a href="listing.php" class="nav-link">Current Intern</a>
             </li>
             <li class="nav-item <?= $activeArray[3] ?>">
                 <a href="testimonies.php" class="nav-link">Testimonies</a>
