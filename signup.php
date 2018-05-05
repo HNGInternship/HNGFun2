2281415443f7a1c8a945f7eef447f00bd5f2018f
chibuokem <chibuokemibezim@gmail.com>
2018-05-05 20:05:29 +0100

    revenge fixing

--- signup.php
+++ signup.php
@@ -1,68 +1,5 @@
 <?php
 include_once("header.php");
-// session_start();
-require_once 'User.php';
-
-$reg_user = new USER();
-
-if ($reg_user->is_logged_in() != "") {
-    $reg_user->redirect('home.php');
-}
-
-if (isset($_POST['btn-signup'])) {
-
-    $firstname = trim($_POST['firstname']);
-    $lastname = trim($_POST['lastname']);
-    $email = trim($_POST['email']);
-    $upass = trim($_POST['password']);
-    $code = md5(uniqid(rand()));
-
-    $stmt = $reg_user->runQuery("SELECT * FROM users WHERE email=:email_id");
-    $stmt->execute(array(":email_id" => $email));
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    // $stmt = $reg_user->runQuery("SELECT * FROM users WHERE userEmail=:email_id");
-	// $stmt->execute(array(":email_id"=>$email));
-	// $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($stmt->rowCount() > 0) {
-        $msg = "
-		      <div class='alert alert-error'>
-				<button class='close' data-dismiss='alert'>&times;</button>
-					<strong>Sorry !</strong>  email allready exists , Please Try another one
-			  </div>
-			  ";
-    } else {
-        if ($reg_user->register($firstname, $lastname, $email, $upass, $code)) {
-            $id = $reg_user->lasdID();
-            $key = base64_encode($id);
-            $id = $key;
-
-            $message = "
-						Hello $firstname $lastname,
-						<br /><br />
-						Welcome to Dragon Slayer!<br/>
-						To complete your registration  please , just click following link<br/>
-						<br /><br />
-						<a href='http://localhost/dragon/verify.php?id=$id&code=$code'>Click HERE to Activate :)</a>
-						<br /><br />
-						Thanks,";
-
-            $subject = "Confirm Registration";
-
-            // $reg_user->send_mail($email,$message,$subject);
-            $msg = "
-					<div class='alert alert-success'>
-						<button class='close' data-dismiss='alert'>&times;</button>
-						<strong>Success!</strong>  We've sent an email to $email.
-                    Please click on the confirmation link in the email to create your account.
-			  		</div>
-					";
-        } else {
-            echo "sorry , Query could no execute...";
-        }
-    }
-}
 ?>
 
 <style>
@@ -82,7 +19,7 @@ if (isset($_POST['btn-signup'])) {
     .link {
         font-size: 14px !important;
     }
-
+    
 </style>
 <div class="container">
 <div class="row h-100">
@@ -93,7 +30,7 @@ if (isset($_POST['btn-signup'])) {
             </span>
             <p style="font-size: 40px !important; text-align: center; color: #2196F3; font-family: 'Qwigley', cursive;">Mark Essien</p>
         </div>
-
+        
     </div>
     <div class="col-md-6  mx-auto">
         <div style='text-align: left'>
@@ -103,9 +40,9 @@ if (isset($_POST['btn-signup'])) {
         <p><span style='color: grey'>Already have an account?</span> <a class='link' href="login.php" style="color: #2196F3; text-decoration: none">Login</a></p>
         </div>
                 <div id="message">
-
+            
                </div>
-
+               
             <form action="" class="text-center" name="register_form" id="register_form">
             <div class="form-row">
                 <div class="form-group col-md-6" style="padding-right:25px">
@@ -117,11 +54,8 @@ if (isset($_POST['btn-signup'])) {
             </div>
 
             <input type="hidden" name="username" id="username" class="form-control" placeholder="User Name">
-        <?php if (isset($msg)) echo $msg; ?>
 
-        <form class="form-signin" method="post">
-
-
+             
             <br />
             <div class="form-row">
                 <div class="form-group col-md-6" style="padding-right:25px">
@@ -138,11 +72,10 @@ if (isset($_POST['btn-signup'])) {
                     </div>
             </div> -->
 
-
+            
             <br />
-
+                
                 <input type="hidden" name="registration" value="yes">
-
 
             
             <div class="form-group">
@@ -152,11 +85,9 @@ if (isset($_POST['btn-signup'])) {
                     I agree to the <a class='link' href="terms-and-conditions.php" style="color: #2196F3; text-decoration: none">Terms and Conditions</a>
                     </label>
                 </div>
-
             </div>
             <br>
            
-
             <button type="submit" name="register" class="btn btn-signup" id="register">Sign Up </button>
         </form>
 
@@ -173,7 +104,7 @@ if (isset($_POST['btn-signup'])) {
             </span>
             <p style="font-size: 40px !important; text-align: center; color: #2196F3; font-family: 'Qwigley', cursive;">Mark Essien</p>
         </div>
-
+        
     </div>
     <div class="col-md-6  mx-auto">
         <div style='text-align: left'>
@@ -183,9 +114,9 @@ if (isset($_POST['btn-signup'])) {
         <p><span style='color: grey'>Already have an account?</span> <a class='link' href="login.php" style="color: #2196F3; text-decoration: none">Login</a></p>
         </div>
                 <div id="message">
-
+            
                </div>
-
+               
             <form action="" class="text-center" name="register_form" id="register_form">
             <div class="form-row">
                 <div class="form-group col-md-6" style="padding-right:50px">
@@ -198,11 +129,11 @@ if (isset($_POST['btn-signup'])) {
 
             <input type="hidden" name="username" id="username" class="form-control" placeholder="User Name">
 
-
+             
             <br />
             <div class="form-row">
                 <div class="form-group col-md-6" style="padding-right:50px">
-                    <input type="email" name="email" id="email" class="form-control" placeholder="Email Address" value="<?php //if(isset($_POST['email'])) { echo $_POST['email']; } ?>">
+                    <input type="email" name="email" id="email" class="form-control" placeholder="Email Address" value="<?php if(isset($_POST['email'])) { echo $_POST['email']; } ?>">
                 </div>
                 <div class="form-group col-md-6" style="padding-right:50px">
                     <input type="text" name="phone" id="phone" class="form-control" placeholder="Phone Number">
@@ -218,7 +149,7 @@ if (isset($_POST['btn-signup'])) {
                     </div>
         </div>
 
-
+            
             <br />
              <div class="form-row">
                         <div class="form-group col-md-6" style="padding-right:50px">
@@ -230,7 +161,7 @@ if (isset($_POST['btn-signup'])) {
                     </div>
                 <input type="hidden" name="registration" value="yes">
 
-
+            
             <br />
             <div class="form-group">
                 <div class="form-check">
@@ -241,7 +172,7 @@ if (isset($_POST['btn-signup'])) {
                 </div>
             </div>
             <br>
-
+           
             <button type="submit" name="register" class="btn btn-signup" id="register">Sign Up </button>
         </form>
 
@@ -257,9 +188,9 @@ if (isset($_POST['btn-signup'])) {
         </p>
         <p><span style='color: grey'>Already have an account?</span> <a class='link' href="login.php" style="color: #2196F3; text-decoration: none">Login</a></p>
                 <div id="message">
-
+            
                </div>
-
+               
             <form action="" class="text-center" name="register_form" id="register_form">
             <div class="form-row">
                 <div class="form-group col-md-6" style="padding-right:50px">
@@ -272,11 +203,11 @@ if (isset($_POST['btn-signup'])) {
 
             <input type="hidden" name="username" id="username" class="form-control" placeholder="User Name">
 
-
+             
             <br />
             <div class="form-row">
                 <div class="form-group col-md-6" style="padding-right:50px">
-                    <input type="email" name="email" id="email" class="form-control" placeholder="Email Address" value="<?php // if(isset($_POST['email'])) { echo $_POST['email']; } ?>">
+                    <input type="email" name="email" id="email" class="form-control" placeholder="Email Address" value="<?php if(isset($_POST['email'])) { echo $_POST['email']; } ?>">
                 </div>
                 <div class="form-group col-md-6" style="padding-right:50px">
                     <input type="text" name="phone" id="phone" class="form-control" placeholder="Phone Number">
@@ -292,7 +223,7 @@ if (isset($_POST['btn-signup'])) {
                     </div>
         </div>
 
-
+            
             <br />
              <div class="form-row">
                         <div class="form-group col-md-6" style="padding-right:50px">
@@ -304,7 +235,7 @@ if (isset($_POST['btn-signup'])) {
                     </div>
                 <input type="hidden" name="registration" value="yes">
 
-
+            
             <br />
             <div class="form-group">
                 <div class="form-check">
@@ -315,7 +246,7 @@ if (isset($_POST['btn-signup'])) {
                 </div>
             </div>
             <br>
-
+           
             <button type="submit" name="register" class="btn btn-signup" id="register">Sign Up </button>
         </form>
 
@@ -323,7 +254,7 @@ if (isset($_POST['btn-signup'])) {
 </div>
 -->
 <script type="text/javascript">
-
+    
        $( document ).ready(function() {
     $("#register").click(function(e){
         e.preventDefault();
@@ -332,9 +263,9 @@ if (isset($_POST['btn-signup'])) {
          var lastname = $("#lastname").val();
         var email = $("#email").val();
          var password = $("#password").val();
-
-        var terms = $('#terms').is(':checked');
-
+        
+        var terms = $('#terms').is(':checked'); 
+        
         if(firstname ==""){
             alert('please enter your firstname');
             $("#message").addClass('alert alert-danger');
@@ -350,7 +281,7 @@ if (isset($_POST['btn-signup'])) {
             $("#message").addClass('alert alert-danger');
             $("#message").html('Please enter email');
         }
-
+        
         else if(password ==""){
             alert('Please enter password');
             $("#message").addClass('alert alert-danger');
@@ -363,8 +294,8 @@ if (isset($_POST['btn-signup'])) {
             $("#message").html('Terms and conditions not accepted by you');
         }
         else{
-
-
+            
+              
       $("#register").html('Registering..');
 
              var data = $("#register_form").serialize();
@@ -381,13 +312,13 @@ if (isset($_POST['btn-signup'])) {
             $("#register").html('Registration successful');
 
             window.location ="dashboard.php";
-             }
+             }  
              else{
                 alert(data);
                 $("#message").html(data);
                  $("#register").html('Failed');
-             }
-
+             } 
+            
 
             },
            error : function(jqXHR,textStatus,errorThrown){
@@ -404,17 +335,17 @@ if (isset($_POST['btn-signup'])) {
             $("#register").html('Registering..');
             },
         });
-
+    
 
         }
-
+        
      });
 
 
 
     });
-
+    
 </script>
 <?php
-include_once("footer.php");
+    include_once("footer.php");
 ?>
