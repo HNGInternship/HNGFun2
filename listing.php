b8f81d911552c30df0727c3c8385ab21eec67b2e
Everistus Olumese <everistusolumese@gmail.com>
2018-05-05 11:28:12 +0100

    Listing.php

--- listing.php
+++ listing.php
@@ -10,7 +10,7 @@ $q = $conn->query($sql);
 $q->setFetchMode(PDO::FETCH_ASSOC);
 $data = $q->fetchAll();
 
-var_dump($data);
+
 ?>
 
 <style>
@@ -59,20 +59,7 @@ var_dump($data);
   <hr style="width: 58px; border-top: 2px solid #3D3D3D;" class="mx-auto mb-5" />
   <p class="text-center my-1">HNG4.0 has been a life-transforming journey for interns across Africa.</p>
   <p class="text-center my-1">Don't take our word for it...take theirs.</p>
-  <form class="form-inline">
-    <div class="row mx-0 mt-4 justify-space-between w-100 mb-5">
-      <select class="form-control col-sm-2" >
-        <option>Skills</option>
-      </select>
-      <select class="form-control col-sm-2" >
-        <option>Country</option>
-      </select>
-      <input class="form-control col-sm-2" type="search" placeholder="Search Name" aria-label="Search" >
-
-      <button class="btn btn-primary col-sm-1" type="submit" >Search</button>
-      <button class="btn btn-outline-primary col-sm-1" type="submit" >Clear</button>
-    </div>
-  </form>
+  
   <?php $total = count($data);
   foreach ($data as $index => $list) { ?>
   <?php if ($index % 3 == 0) {
@@ -86,14 +73,15 @@ $count++;
 ?>
     <div class="profile">
       <div class="card">
-        <img class="card-img-top" src="<?= $list['image_filename'] ?>" onerror="this.src='images/default.jpg'" alt="Profile Image">
+        <img class="card-img-top" src="<?php echo $list['image_filename'] ?>" onerror="this.src='images/default.jpg'" alt="Profile Image">
         <div class="card-footer">
-          <a href="profile.php?id=<?= $list['username'] ?>" class="ml-3 my-0 py-0 btn btn-default">View Profile</a>
+          <a href="profile.php?id=<?php echo $list['username'] ?>" class="ml-3 my-0 py-0 btn btn-default">View Profile</a>
           <i class="fa fa-github fa-lg"></i>
         </div>
+      
       </div>
-      <h4 class="text-center mt-3"><?= $list['username'] ?></h4>
-      <p class="small text-center mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+      <h4 class="text-center mt-3"><?php echo  $list['firstname'] . " " . $list['lastname'] ?></h4>
+     
     </div>
   <?php if ($count === 3 || $index == $total - 1) { ?>
   </div>
