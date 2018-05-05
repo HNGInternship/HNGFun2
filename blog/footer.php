1661c848430f180551a06beb65f897c0fa911512
Dennis W. Otugo <otugodennis@gmail.com>
2018-05-05 18:25:31 +0100

    Add files via upload

--- blog/footer.php
+++ blog/footer.php
@@ -1,19 +1,55 @@
-			</div>
-		</div>
-		<div id="footer-wrapper">
-			<div class="container">
-				<div class="row">
-					<div class="12u">
-						<div id="copyright">
-							<a href="http://www.thesoftwareguy.in" target="_blank">&copy; 2013 - <?php echo date("Y"); ?> www.thesoftwareguy.in</a> All rights Reserved. | Template taken from: <a href="http://html5up.net"> &copy; HTML5 UP</a> 
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	<!-- ********************************************************* -->
-	<script src="js/config.js"></script>
-    <script src="js/skel.min.js"></script>
-    <script src="js/skel-panels.min.js"></script>
-	</body>
+<footer>
+   <div class="container footer">
+      <div class="row">
+         <div class="col-lg-8 col-md-10 mx-auto">
+            <ul class="list-inline text-center">
+               <li class="list-inline-item contact-icon">
+                  <a id="twitter" href="https://twitter.com/hnginternship?lang=en" target="_blank">
+                     <span>
+                        <i class="fa fa-twitter fa-lg"></i>
+                     </span>
+                  </a>
+               </li>
+               <li class="list-inline-item contact-icon" style="border: 1px solid black;border-top-style: none;border-bottom-style: none; ">
+                  <a id="facebook" href="https://web.facebook.com/hotelsng/" target="_blank">
+                     <span>
+                        <i class="fa fa-facebook fa-lg"></i>
+                     </span>
+                  </a>
+               </li>
+               <li class="list-inline-item contact-icon">
+                  <a id="github" href="https://github.com/HNGInternship/" target="_blank">
+                     <span>
+                        <i class="fa fa-github fa-lg"></i>
+                     </span>
+                  </a>
+               </li>
+    
+            </ul>
+            <div style="text-align: center">
+                <p class="copyright text-muted">Copyright &copy; HNG FUN 2018</p> 
+            </div>
+
+</footer>
+
+ <!-- Bootstrap core JavaScript -->
+<!-- <script src="vendor/jquery/jquery.min.js"></script>-->
+ <script src="https://hng.fun/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="https://hng.fun/js/hng.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+ <!-- Custom scripts for this template -->
+ <script src="https://hng.fun/js/hng.min.js"></script>
+
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+</body>
+
+
 </html>
