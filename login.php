c096afcc50cf45f30353b7140603391072517376
Michael Ajetunmobi <mikoloxtra@gmail.com>
2018-05-05 05:21:55 +0100

    db fix

--- login.php
+++ login.php
@@ -11,7 +11,7 @@ $username = DB_USER;
 $password = DB_PASSWORD;
 $database = DB_DATABASE;
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn = new PDO("mysql:host=$servername; dbname=$database", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
