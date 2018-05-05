084b5f6750e8a05ada828f56023610972538575c
Dennis W. Otugo <otugodennis@gmail.com>
2018-05-05 01:12:45 +0100

    Update db.php

--- db.php
+++ db.php
@@ -3,6 +3,36 @@
 DO NOT MODIFY THIS FILE!!!
  */
 require '../config.php';
+// display all error except deprecated and notice  
+error_reporting( E_ALL & ~E_DEPRECATED & ~E_NOTICE );
+// turn on output buffering 
+ob_start();
+
+/*
+if you make login section for admin 
+use session here otherwise no need
+session_start();
+*/
+
+require_once("blog/public_functions.php");
+
+/*
+ * turn off magic-quotes support, for runtime e, as it will cause problems if enabled
+ */
+if (version_compare(PHP_VERSION, 5.3, '<') && function_exists('set_magic_quotes_runtime')) set_magic_quotes_runtime(0);
+
+// set currentPage in the local scope
+$currentPage = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+
+
+// basic options for PDO 
+$dboptions = array(
+    PDO::ATTR_PERSISTENT => FALSE,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+);
+
 
 try {
     $db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE, DB_USER, DB_PASSWORD);
