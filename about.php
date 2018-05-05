df86e2ec60bb51a5d06a705acb822097b29fdfe7
Ogoluwa Ojewale <simeondgenius@gmail.com>
2018-05-05 18:06:11 +0100

    @Ogopedia - Implemented the contact form

--- about.php
+++ about.php
@@ -93,7 +93,7 @@ include_once("header.php");
 
 <main>
 	<div class="text-white img-bg">
-        <img src="/img-light.jpeg" alt="" style= "height: 600px">
+        <img src="/img-light.jpeg" alt="" style= "height: 500px; width: 1000px">
 			<h1 class="mb-2 head1">What is HNG all about?</h1>
 			<p class="head" >The HNG is a 3-month remote internship program designed to locate the most talented software developers in Nigeria and the whole of Africa</p>
 	</div>
