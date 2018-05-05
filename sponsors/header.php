5645b2666dcab5aa4446f6014d1a86e8b4c46447
bogadeji <bogadeji@gmail.com>
2018-05-05 13:48:02 +0100

    created partner pages, edited sponsors.php

---
+++ sponsors/header.php
@@ -0,0 +1,122 @@
+<?php
+  include('../header.php');
+?>
+
+      <style>
+        
+     p {
+       font-size: 14px; 
+    }
+    
+    .rightColumn {
+        padding: 50px 5px 5px 20px;
+		
+    }
+    ul {
+    list-style-type: circle;
+    }
+	
+    
+	
+	.voffset {
+	margin-top: 330px; 
+	
+	}
+	
+	span{
+	  font-size: 14px; 
+    }
+	
+	.listing{
+		
+		font-size: 14px; 
+	}
+	.cont{
+		
+		padding-top: 20px;
+		padding-left: 10px;
+		padding-bottom: 10px;
+		background-color: #ffffff;
+		width: 100%;
+		
+	}
+	
+	.head{
+		margin-top: 40px;
+		margin-left: 40px;
+		color: #ffffff;
+		}
+	.circle{
+		width: 43px;
+		height: 43px;
+		border-radius: 50%;
+		background-color: #48BBFC;
+		z-index: 100;
+	}
+	.btn{
+		margin-top: -130px;
+		background-color: #ffffff;
+		border-radius: 50px;
+		color: #48BBFC;
+		margin-left: 100px;
+		width: 181px;
+		height: 39px;
+	}
+	#img-fixed {
+   background: blue;
+   position: absolute;
+				top: 40%;
+				left: 7%;/*
+   position: relative;*/
+   width: 33.3333%;
+}
+#img-fixed img{
+  width: 100%;
+  height: 70vh;
+}
+.img-partner{
+  	 position: relative;
+    display: inline-block;
+  }
+  @media (min-width: 575px) {
+  #img-fixed {
+    width: 235px; 
+  }
+  .img-partner{
+  	margin-top: 300px;
+  	 position: relative;
+    display: inline-block;
+  }
+}
+@media (min-width: 768px) {
+  #img-fixed {
+    width: 235px; 
+  }
+  .img-partner{
+  	margin-top: 50px;
+  	 position: relative;
+    display: inline-block;
+  }
+}
+@media (min-width: 992px) {
+  #img-fixed {
+    width: 309px; 
+  }
+  .img-partner{
+  	 position: relative;
+    display: inline-block;
+  }
+}
+@media (min-width: 1200px) {
+  #img-fixed {
+    width: 375px; 
+  }
+  .img-partner{
+  	 position: relative;
+    display: inline-block;
+  }
+} 
+    </style>
+
+
+
