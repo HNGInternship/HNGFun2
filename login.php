11c08e1ff3fe11a801ae51961c5c3d3b9569848a
Michael Ajetunmobi <mikoloxtra@gmail.com>
2018-05-05 05:10:06 +0100

    db fix

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ ob_start();
 session_start();
 
 define ('DB_USER', "root");
-define ('DB_PASSWORD', "");
+define ('DB_PASSWORD', "29gE9t*dJ2#2f-BS");
 define ('DB_DATABASE', "dragons_shield");
 define ('DB_HOST', "localhost");
 $servername = DB_HOST;
