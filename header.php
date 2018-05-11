4bef1dbe813a8450bed36cfe47883b366778d72a
bogadeji <bogadeji@gmail.com>
2018-05-11 18:48:30 +0100

    adjusted header

--- header.php
+++ header.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION)) { session_start(); }
 
 $fileName=basename($_SERVER['PHP_SELF']);
 
-$files = array('index','learn','listing','testimonies','sponsors','alumni','partners', 'admin', signup, login);
+$files = array('index','learn','listing','testimonies','sponsors','alumni','partners', 'admin', 'signup', 'login');
 
 $activeArray = array('','','','','','','','','');
 
@@ -159,7 +159,7 @@ $activeArray[$fileIndex]="active";
             </li>
             <li class="nav-item <?= $activeArray[1] ?>">
                 <a href="learn" class="nav-link">Learn</a>
-            </li><?php if($_SERVER['REQUEST_URI'] === "listing"){ echo 
+            </li><?php if($_SERVER['REQUEST_URI'] === "/listing/"){ echo 
             '<li class="nav-item <?= $activeArray[6] ?>">
                 <a href="admin" class="nav-link">Register</a>
             </li>';}?>
