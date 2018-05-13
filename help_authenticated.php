1a6d27d0f378643935fe3613d2f29450364bc1f5
John Olubori David <daveholuborhee@gmail.com>
2018-05-13 11:57:07 +0100

    Revert listing to make use of general header.php

---
+++ help_authenticated.php
@@ -0,0 +1,39 @@
+<?php
+include_once("header.php");
+?>
+<style>
+  h2 {
+    font-family: 'work sans';
+  }
+  .profile {
+    width: 250px;
+  }
+  .card-img-top {
+    height: 250px !important;
+  }
+  /* #contain img{
+    width:100%;
+    padding:5px;
+    height:200px;
+   }
+   #contain{
+     margin-left:auto;
+     margin-right:auto;
+   }
+   #contain #border{
+     margin-left:70px;
+     margin-right:70px;
+     padding:0;
+     width: 250px;
+     border: solid 1px #E5E5E5;
+   }
+   #caption{
+     text-align:center;
+   }*/
+</style>
+<main class="container mt-5 mb-5 px-5">
+  help page for logged in user
+</main>
+<?php
+include_once("footer.php");
+?>
