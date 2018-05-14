b9a0d4045c2dcf054a7b58ffedfe2a2a9525819e
dmaaj <jamuheez2009@gmail.com>
2018-05-14 09:17:07 +0100

    fixed conflict

--- coin_header.php
+++ coin_header.php
@@ -44,6 +44,7 @@ $activeArray[$fileIndex]="active";
 
     <!-- Bootstrap core CSS -->
       <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
    <script src="js/jquery.min.js"></script>
 
