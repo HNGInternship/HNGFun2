df5575096a2af5f452d8120e12a7263116759d1a
Epospiky <epospiky@gmail.com>
2018-05-15 00:05:21 +0100

    editted reset password notification

--- resetpassword.php
+++ resetpassword.php
@@ -7,10 +7,10 @@ include_once("header.php");
 
 <div class="container" >
     
-
+<div class="clearfix"></div>
     <div class="row justify-content-md-center">
-        <div class="clearfix"></div>
-        <div id="notif" class="contnt"></div>
+        
+        <div id="notif" ></div>
 
         <form class="contnt" id="reset_form">
             <h1>Reset Password</h1>
