6590137adb4257113e36767732c637b052bf57f0
John Olubori David <daveholuborhee@gmail.com>
2018-05-14 06:34:13 +0100

    Make call to db.php in olubori.php

--- profiles/olubori.php
+++ profiles/olubori.php
@@ -1,15 +1,15 @@
 <?php 
    if(isset($_GET['answer'])){
 
-		require_once '../../config.php';
+		/*require_once '../../config.php';
     
        try {
 		    $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
 		} catch (PDOException $pe) {
 		    die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-		}
+		}*/
 		
-		
+		require_once '../db.php';
 
 		$question = safeInput($_GET['question']);
 		$answer = safeInput($_GET['answer']);
