6d6f94ac0f3f88070be930a59461b84f4ffbb2a9
dmaaj <jamuheez2009@gmail.com>
2018-05-17 15:25:54 +0100

    fixed coin_header

--- coin_header.php
+++ coin_header.php
@@ -42,7 +42,7 @@ $activeArray[$fileIndex]="active";
 
 
 
-   <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+   <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <!-- <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'> -->
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <!-- <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet"> -->
