6952e2aa29557e5237df9338517faab9a5abd28f
Epospiky <epospiky@gmail.com>
2018-05-17 22:30:56 +0100

    fixed reset password

--- resetpassword.php
+++ resetpassword.php
@@ -4,11 +4,11 @@ include_once("header.php");
 <style>
     .contnt{text-align: center; padding: 100px;}
 </style>
-<div class="clearfix"></div>
+
 <div class="container" >
     
-
-    <div class="justify-content-md-center">
+<div class="clearfix"></div>
+    <div class="row justify-content-md-center">
         
         <div id="notif" ></div>
 
@@ -39,7 +39,7 @@ include_once("header.php");
         var email = $("#email").val();
              
         if(email ==""){
-           
+            alert('please enter email');
             $("#notif").addClass(' alert alert-danger');
             $("#notif").html('Please enter email');
         }
@@ -62,14 +62,13 @@ include_once("header.php");
              
              $("#notif").addClass('alert alert-success');
             $("#notif").html(data);
-              $("#notif").html('Password reset successful! Please check your email and proceed.');
+
             $("#reset").html('DONE');          
 
             },
            error : function(jqXHR,textStatus,errorThrown){
                  if(textStatus ='error'){
-                  
-                      $("#notif").html('Email does not exist!');
+                    alert('Request not completed');
                  }
                 $("#reset").html('Failed');
             },
