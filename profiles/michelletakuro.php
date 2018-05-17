4a9be03af69b822a5f11c09e07738b663824b38b
unknown <mitchelletakuro@gmail.com>
2018-05-17 14:24:53 +0100

    inor changes

--- profiles/michelletakuro.php
+++ profiles/michelletakuro.php
@@ -1,6 +1,6 @@
 <?php
 	if(!defined('DB_USER')){
-		require "C:\laragon\www\config.php";
+		require "../../config.php";
     }
     try {
 		$conn = new PDO("mysql:host=".DB_HOST."; dbname=".DB_DATABASE, DB_USER, DB_PASSWORD);
