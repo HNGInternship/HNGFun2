cff83ed7647308fbac7e661cf7c6ee0e1c6be5ec
Legendary <Legendary-tony>
2018-05-18 09:41:11 +0100

    updated

--- profiles/Uju.php
+++ profiles/Uju.php
@@ -1,8 +1,219 @@
-<!DOCTYPE 
-html>
+<!DOCTYPE html>
 <html>
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Alegreya|Allura|Almendra SC|Romanesco|Source+Sans+Pro:400,700' rel='stylesheet'>
+    <link href="https://static.oracle.com/cdn/jet/v4.0.0/default/css/alta/oj-alta-min.css" rel="stylesheet" type="text/css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 
-<?php
+    <style type="text/css">
+
+        .container{
+            width: 100%;
+            min-height: 100%
+        }
+        .body0 {
+            height: 100%;
+        }
+
+        span {
+            display: inline-block;
+            vertical-align: middle;
+            line-height: normal;
+        }
+
+        .main {
+            position: relative;
+            /*top:20px;*/
+            width: 100%;
+            /*padding-top: 300px;*/
+            max-height: 230px;
+            font-family: "Romanesco";
+            line-height: 150px;
+            font-size: 96px;
+            text-align: center;
+        }
+        .text {
+            background: -webkit-linear-gradient(0deg, #FF0F00, rgba(17, 26, 240, 0.55), #EC0F13);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+        .under {
+            position: relative;
+            /*top:450px;*/
+            max-height: 100px;
+            width: 100%;
+            font-family: "Alegreya";
+            line-height: normal;
+            font-size: 32px;
+            text-align: center;
+            color: #000830;
+        }
+        .under1 {
+            position: relative;
+            /*top:500px;*/
+            height: 40px;
+            width: 100%;
+            font-family: "Alegreya";
+            line-height: normal;
+            font-size: 32px;
+            text-align: center;
+            color: #000830;
+        }
+        .under2 {
+            position: relative;
+            /*top:540px;*/
+            height: 49.71px;
+            width: 100%;
+            font-family: "Alegreya";
+            line-height: normal;
+            font-size: 32px;
+            text-align: center;
+            color: #000830;
+            background-color: #1380FA;
+        }
+        body{
+            background-image: url(boats.jpg);
+            background-size: cover;
+        }
+        }
+        .body1 {
+            font-family: 'Source Sans Pro', sans-serif;
+            font-size: 75%;
+            display: flex;
+            flex-direction: column;
+            max-width: 700px;
+            margin: 0 auto;
+        }
+        .chat-output {
+            flex: 1;
+            padding: 20px;
+            display: flex;
+            background: white;
+            flex-direction: column;
+            overflow-y: scroll;
+            max-height: 500px;
+        }
+        .chat-output > div {
+            margin: 0 0 20px 0;
+        }
+        .chat-output .user-message .message {
+            background: #0fb0df;
+            color: white;
+        }
+        .chat-output .bot-message {
+            text-align: right;
+        }
+        .chat-output .bot-message .message {
+            background: #eee;
+        }
+        .chat-output .message {
+            display: inline-block;
+            padding: 12px 20px;
+            border-radius: 10px;
+        }
+        .chat-input {
+            padding: 20px;
+            background: #eee;
+            border: 1px solid #ccc;
+            border-bottom: 0;
+        }
+        .chat-input .user-input {
+            width: 100%;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            padding: 8px;
+        }
+        .send_button{
+      width: 75px;
+      height: 35px;
+      padding: 5px;
+      margin-top: 8px;
+      color: black;
+      border:none;
+      border-radius: 5px;
+      background-color: #1380FA;
+    }
+        
+
+    </style>
+</head>
+<body>
+<div class="container">
+    <?php
+
+    global $conn;
+
+    try {
+        $sql2 = 'SELECT * FROM interns_data WHERE username="Uju"';
+        $q2 = $conn->query($sql2);
+        $q2->setFetchMode(PDO::FETCH_ASSOC);
+        $my_data = $q2->fetch();
+    } catch (PDOException $e) {
+        throw $e;
+    }
+    ?>
+
+
+
+    <div class="oj-flex oj-flex-items-pad oj-contrast-marker">
+        <div class="oj-sm-12 oj-md-6 oj-flex-item">
+            <div class="oj-flex oj-sm-align-items-center oj-sm-margin-2x">
+                <div role="img" class="oj-flex-item alignCenter">
+                    <oj-avatar role="img" size="[[avatarSize]]" initials='[[initials]]'
+                    data-bind="attr:{'aria-label':'Avatar of Melody Okunuga'}">
+                    </oj-avatar>
+                    <img class="img-fluid " onerror="this.src='https://res.cloudinary.com/uuujuuu/image/upload/v1526014279/IMG_0681.jpg'" src="<?=$my_data['image_filename'] ?>" >
+                </div>
+            </div>
+            <div class="body0">
+                <div class="main"><span class="text"><?=$my_data['name'] ?></span></div>
+                <div class="under"><span>Front End Web Developer</span></div>
+                <div class="under1"><span>
+                        <div class="oj-flex oj-md-align-items-center"><a href="https://github.com/mokunuga">
+                            <div class="oj-flex-item oj-flex oj-sm-flex-direction-column oj-sm-align-items-center oj-sm-margin-2x">
+                                <img style="width:40px; height: 40px;" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-512.png">
+                            </div></a>
+
+                            <a href="https://linkedin.com/in/mokunuga">
+                                 <div class="oj-flex-item oj-flex oj-sm-flex-direction-column oj-sm-align-items-center oj-sm-margin-2x">
+                                    <img style="width:40px; height: 40px;" src="http://icons.iconarchive.com/icons/custom-icon-design/pretty-social-media/256/Linkedin-icon.png">
+                                 </div>
+                            </a>
+                        </div></span>
+                </div>
+                <br>
+                <br>
+            </div>
+        </div>
+        
+      <br>
+      <br>
+      <br>
+        <div class="oj-sm-12 oj-md-6 oj-flex-item">
+          <div class="under2"><span>Juu Bot</span></div>
+            <div class="body1">
+                <div class="chat-output" id="chat-output">
+                    <div class="user-message">
+                        <div class="message">Hello! My name is Juu bot.<br>I'm willing to assist you with any of your questions.<br>Type <span style="color: #FABF4B;"><strong> aboutbot</strong></span> to know more about me. </br>To train me, use this format - 'train: question # answer # password'. </br>To learn more about me, simply type - 'aboutbot'.</div>
+                    </div>
+                </div>
+
+                <div class="chat-input">
+                    <form action="" method="post" id="user-input-form">
+                        <input type="text" name="user-input" id="user-input" class="user-input" placeholder="Say something here">
+                        <button type="submit" class="send_button" id="send">Send</button>
+                    </form>
+                </div>
+            
+
+            </div>
+        </div>
+    </div>
+  
+
+
+
+    <?php
 
     try {
         $sql = 'SELECT * FROM secret_word';
@@ -15,7 +226,7 @@ html>
     $secret_word = $data['secret_word'];
 
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $data = $_POST['input_text'];
+        $data = $_POST['user-input'];
       //  $data = preg_replace('/\s+/', '', $data);
         $temp = explode(':', $data);
         $temp2 = preg_replace('/\s+/', '', $temp[0]);
@@ -30,7 +241,7 @@ html>
     }
 
     function aboutbot() {
-        echo "<div id='result'>My  name is Juu-Bot v1.0 - I'm like human with fast brain of understanding, I get input and process it in other to display the result, if there is no result you can instruct me on how to get such result!</div>";
+        echo "<div id='result'>Juu Bot v1.0 - I am simply a bot that returns data from the database and I also can be taught new tricks!</div>";
     }
     function train($input) {
         $input = explode('#', $input);
@@ -56,7 +267,7 @@ html>
                 try {
                     $q = $GLOBALS['conn']->prepare($sql);
                     if ($q->execute($training_data) == true) {
-                        echo "<div id='result'>Thank you for teaching me new words, it seem you are very good in lecturing. I will like you to teach me more next time!</div>";
+                        echo "<div id='result'>Training Successful!</div>";
                     };
                 } catch (PDOException $e) {
                     throw $e;
@@ -77,7 +288,7 @@ html>
         $q->setFetchMode(PDO::FETCH_ASSOC);
         $data = $q->fetchAll();
         if(empty($data)){
-            echo "<div id='result'>Sorry, I don not understand what you meant. You can teach me simply by using the format - 'train: question # answer # password' (without quote)</div>";
+            echo "<div id='result'>Sorry, I do not know that command. You can train me simply by using the format - 'train: question # answer # password'</div>";
         }else {
             $rand_keys = array_rand($data);
             echo "<div id='result'>". $data[$rand_keys]['answer'] ."</div>";
@@ -85,260 +296,41 @@ html>
     }
     ?>
 
-<head>
-<!--<link rel="stylesheet" href="https://res.cloudinary.com/uuujuuu/image/upload/v1526014279/IMG_0681.jpg"> -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-  <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
-   <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One|Ubuntu" rel="stylesheet">
-<style>
-body{
- background-image: url(boats.jpg);
- background-size: cover;
-}
-.card {
- box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
- max-width: 300px;
- margin: auto;
- text-align: center;
- font-family: arial;
-}
-h1{
-  color: black;
-}
-.title {
- color: black;
- font-size: 18px;
-}
-button {
- border: none;
- outline: 0;
- display: inline-block;
- padding: 8px;
- color: white;
- background-color: #000;
- text-align: center;
- cursor: pointer;
- width: 100%;
- font-size: 18px;
-}
-a {
- text-decoration: none;
- font-size: 22px;
- color: black;
-}
-button:hover, a:hover {
- opacity: 0.7;
-}
-
-    .chatbot-container{
-      background-color: #F3F3F3;
-      width: 400px;
-      height: 500px;
-      margin: 10px;
-      box-sizing: border-box;
-      box-shadow: -3px 3px 5px gray;
-    }
-    .chat-header{
-      width: 400px;
-      height: 50px;
-      background-color: #1380FA;
-      color: white;
-      text-align: center;
-      padding: 10px;
-      font-size: 1.5em;
-    }
-    #chat-body{     
-        display: flex;
-        flex-direction: column;
-        padding: 10px 20px 20px 20px;
-        background: white;           
-        overflow-y: scroll;
-        height: 395px;
-        max-height: 395px;
-    }
-    .message{
-      background-color: #1380FA;
-      color: black;
-      font-size: 0.8em;
-      width: 300px;
-      display: inline-block;
-      padding: 10px;
-      margin: 5px;
-      border-radius: 10px;
-      line-height: 18px;
-    }
-    .bot_chat{
-      text-align: left;
-
-    }
-    .bot_chat .message{
-      background-color: #1380FA;
-      color: black;
-      opacity: 0.9;
-      box-shadow: 3px 3px 5px gray;
-    }
-    .user_chat{
-      text-align: right;
-    }
-    .user_chat .message{
-      background-color: #E0E0E0;
-      color: black;
-      box-shadow: 3px 3px 5px gray;
-    }
-    .chat-footer{
-      background-color: #F3F3F3;
-    }
-    .input-text-container{
-      margin-left: 20px;
-    }
-    .input_text{      
-      width: 300px;
-      height: 35px;
-      padding: 5px;
-      margin-top: 8px;
-      border: 0.5px solid #1380FA;
-      border-radius: 5px;
-    }
-    .send_button{
-      width: 75px;
-      height: 35px;
-      padding: 5px;
-      margin-top: 8px;
-      color: black;
-      border:none;
-      border-radius: 5px;
-      background-color: #1380FA;
-    }.myfooter{
-      margin: 100px 0px 50px 0px;
-      font-size: 0.9em;
-    }.footer_line{
-      padding: 0px;
-      margin-bottom: 20px;
-      border: 0.5px solid #34495E;
-      background-color: #34495E;
-      width: 100%;
-    }
-    .grey-text{
-      text-decoration: none;
-    }
-</style>
-</head>
-<?php
-
-    if(!defined('DB_USER')){
-    require "../../config.php"; 
-    try {
-        $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
-    } catch (PDOException $pe) {
-        die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
-    }
-  }
-
-    try {
-        $sql2 = 'SELECT * FROM interns_data WHERE username="Uju"';
-        $q2 = $conn->query($sql2);
-        $q2->setFetchMode(PDO::FETCH_ASSOC);
-        $my_data = $q2->fetch();
-    } catch (PDOException $e) {
-        throw $e;
-    }
-    ?>
-<body>
-    <br>
-    <br>
-    <br>
-
-
-
-     <div class="container">
-    <div class="row wow fadeInLeft" data-wow-delay=".3s">
-      <div class="col-lg-6 col-xl-5 pr-lg-5 pb-5"><div class="bot_chat">
-        <div class="message">
-          <p class="grey-text">
-
-            <!-- Profile start -->
-            <h2 style="text-align:center">Profile</h2>
-
-<div class="card">
- <img src="https://res.cloudinary.com/uuujuuu/image/upload/v1526014279/IMG_0681.jpg" alt="Uuujuuu" style="width:100%">
- <h1>Username: <?=$my_data['username'] ?> </h1>
- <p class="title">Front End Web Developer</p>
- <div style="margin: 24px 0;">
-
-   <a href="#"><i class="fa fa-twitter"></i></a>  
-   <a href="#"><i class="fa fa-linkedin"></i></a  
-   <a href="#"><i class="fa fa-facebook"></i></a>
-
 </div>
-<p><button>Contact</button></p>
-</div>
-<!-- Profile end -->
 
-      </div></div> </b></div>
-      <div class="col-lg-6 col-xl-7 pl-lg-5 pb-4">
-        <div class="row mb-3">
-          <div class="col-10">
-            <p class="grey-text">
-             <div class="chatbot-container">
-
-                <!-- CHAT BOT HERE -->
-      <div class="chat-header">
-       <h2 style="text-align:center">Juu Bot</h2>
-      </div>
-      <div id="chat-body">
-        <div class="bot_chat">
-            <div class="message">Hello! My name is Juu bot.<br>I'm willing to assist you with any of your questions.<br>Type <span style="color: #FABF4B;"><strong> aboutbot</strong></span> to know more about me.
-            </div>
-            
-        </div>
-      </div>
-      <div class="chat-footer">
-        <div class="input-text-container">
-          <form action="" method="post" id="chat-input-form">
-            <input type="text" name="input_text" id="input_text" required class="input_text" placeholder="Type your question here...">
-            <button type="submit" class="send_button" id="send">Send</button>
-          </form>
-        </div>        
-      </div>
-    </div>
-  </div>  
+</body>
 
 
-<!--My script here-->
 <script>
-    var outputArea = $("#chat-body");
+    var outputArea = $("#chat-output");
 
-    $("#chat-input-form").on("submit", function(e) {
+    $("#user-input-form").on("submit", function(e) {
 
         e.preventDefault();
 
-        var message = $("#input_text").val();
+        var message = $("#user-input").val();
 
-        outputArea.append(`<div class='user_chat'><div class='message'>${message}</div></div>`);
+        outputArea.append(`<div class='bot-message'><div class='message'>${message}</div></div>`);
 
 
         $.ajax({
             url: 'profile.php?id=Uju',
             type: 'POST',
-            data:  'input_text=' + message,
+            data:  'user-input=' + message,
             success: function(response) {
                 var result = $($.parseHTML(response)).find("#result").text();
                 setTimeout(function() {
-                    outputArea.append("<div class='bot-chat'><div class='message'>" + result + "</div></div>");
-                    $('#chat-body').animate({
-                        scrollTop: $('#chat-body').get(0).scrollHeight
+                    outputArea.append("<div class='user-message'><div class='message'>" + result + "</div></div>");
+                    $('#chat-output').animate({
+                        scrollTop: $('#chat-output').get(0).scrollHeight
                     }, 1500);
                 }, 250);
             }
         });
 
 
-        $("#input_text").val("");
+        $("#user-input").val("");
 
     });
 </script>
 
-
-</body>
-</html>
