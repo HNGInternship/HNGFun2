9e619a31d6b39e0668653c3e3a4a57611934d91d
Obianuju Chibuzor <marvingayec@gmail.com>
2018-05-18 15:31:31 +0100

    bot

--- profiles/Uju.php
+++ profiles/Uju.php
@@ -299,7 +299,7 @@ catch(PDOException $pe)
                 </div>
 
                 <div class="chat-input">
-                    <form>
+                    <form action="" method="post" id="user-input-form">
                         <input type="text" name="user-input" id="user-input" class="user-input" placeholder="Say something here">
                         <button type="submit" class="send_button" id="send">Send</button>
                     </form>
