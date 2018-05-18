3d80ea6d1dba778fa2aad2610dbba6a2bc05171d
Leke-Ariyo <37749047+Leke-Ariyo@users.noreply.github.com>
2018-05-18 23:00:28 +0100

    Update sell_coins.php

--- sell_coins.php
+++ sell_coins.php
@@ -1,6 +1,6 @@
 <?php
 include_once("coin_header.php");
-/*include_once("db.php");
+include_once("db.php");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 if(!isset($_GET['request_id'])){
     echo "<script>alert('Request ID required');</script>";
