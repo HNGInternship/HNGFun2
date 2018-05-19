a03ab44350a720cbbf6be73930fc9ee6bbc7b605
Legendary <Legendary-tony>
2018-05-19 14:57:46 +0100

    updated

--- profiles/michelletakuro.php
+++ profiles/michelletakuro.php
@@ -1,14 +1,5 @@
  <?php
 
-    try {
-        $sql2 = 'SELECT * FROM interns_data WHERE username="michelletakuro"';
-        $q2 = $conn->query($sql2);
-        $q2->setFetchMode(PDO::FETCH_ASSOC);
-        $my_data = $q2->fetch();
-    } catch (PDOException $e) {
-        throw $e;
-    }
-
 if($_SERVER['REQUEST_METHOD'] === 'GET'){
 
     try {
@@ -244,7 +235,7 @@ return ;
                     <oj-avatar role="img" size="[[avatarSize]]" initials='[[initials]]'
                     data-bind="attr:{'aria-label':'Avatar of Melody Okunuga'}">
                     </oj-avatar>
-                    <img class="img-fluid "  src="<?=$my_data['image_filename'] ?>" height="40" width="400" >
+                   <img class="img-fluid " onerror="this.src='https://res.cloudinary.com/uuujuuu/image/upload/v1526014279/IMG_0681.jpg'" src="<?=$my_data['image_filename'] ?>" >
                 </div>
             </div>
             <div class="body0">
