60468127bb842b9d649cd2edc3476667c92a3a0e
Leke-Ariyo <37749047+Leke-Ariyo@users.noreply.github.com>
2018-05-20 22:13:12 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -19,7 +19,7 @@ if (isset($_POST["submit"])) {
     $phone_number = $_POST['phone_number'];
     $subject = $_POST['subject'];
     $from = 'From: $email \r\n'; 
-    $to = 'support@hng.fun',$email; 
+    $to = $email; 
     $body ="From: $name\n E-Mail: $email\n Phone Number: $phone_number\n Message:\n $message";
     // Check if name has been entered
     if (!$_POST['name']) {
