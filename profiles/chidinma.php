ee06246b6df787a83a2203fb6046f35be413d7d7
chidinma <chypearlnel@gmail.com>
2018-05-21 13:29:38 +0100

    new

--- profiles/chidinma.php
+++ profiles/chidinma.php
@@ -294,7 +294,4 @@
     </script>
 
 	</body>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" ></script>
 </html>
