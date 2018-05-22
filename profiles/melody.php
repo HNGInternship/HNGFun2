73266bc687064665a90751f49e98acda7ac3e19d
Okunuga Melody <mokunuga@gmail.com>
2018-05-22 20:44:01 +0100

    Bot changes

--- profiles/melody.php
+++ profiles/melody.php
@@ -297,7 +297,7 @@
             success: function(response) {
                 var result = $($.parseHTML(response)).find("#result").text();
                 console.log(message);
-                console.log(result);
+                console.log($($.parseHTML(response)).find("#result"));
                 setTimeout(function() {
                     outputArea.append("<div class='user-message'><div class='message'>" + result + "</div></div>");
                     $('#chat-output').animate({
