0883bde5e729d62548f6548dc32d7b9cdaf68ebb
ionware <ionwarez@gmail.com>
2018-05-22 11:17:00 +0100

    corrected AJAX request path

--- profiles/ionware.php
+++ profiles/ionware.php
@@ -429,12 +429,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         _response.make(inputNode.val()).send($('.bot-conversation'));
         inputNode.val("");
         $.ajax({
-            url: "/profiles/ionware.php",
+            url: "/profiles/ionware",
             data: { q: question.trim() },
             type: 'POST',
             success: function(data) {
                 var response = new Response("bot");
-                console.log(data.answer);
+                console.log(data);
                 if (!data.answer) {
                     response.make("iBot server is not responding properly").send($(".bot-conversation"));
                 } else {
@@ -443,6 +443,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $('.bot-conversation').animate({ 'scrollTop': $(".bot-conversation")[0].scrollHeight});
             },
             error: function(error) {
+                console.log(error);
                 var response = new Response("bot");
                 response.make("iBot server is not responding properly").send($(".bot-conversation"));
                 console.log(error);
