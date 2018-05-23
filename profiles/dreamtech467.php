e8eb020aaf50aee7d005e3815042d3c22e52c738
realtech <aiboroma467@users.noreply.github.com>
2018-05-23 11:56:10 +0100

    Update dreamtech467.php

--- profiles/dreamtech467.php
+++ profiles/dreamtech467.php
@@ -2,22 +2,16 @@
 <?php
 
 
-	include_once("../answers.php"); 
-	if(!defined('DB_USER')){
-		if (file_exists('../../config.php')) {
-			require_once '../../config.php';
-		} else if (file_exists('../config.php')) {
-			require_once '../config.php';
-		} elseif (file_exists('config.php')) {
-			require_once 'config.php';
-		}
-			
-		try {
-			$conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);			
-		} catch (PDOException $e) {
-			die("Could not connect to the database " . DB_DATABASE . ": " . $e->getMessage());
-		}
-	}
+include_once("../answers.php"); 
+
+if(!defined('DB_USER')){
+     require "../../config.php";
+     try {
+         $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+     } catch (PDOException $pe) {
+         die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+     }
+   }
 
   try {
       $sql = "SELECT * FROM interns_data WHERE username ='dreamtech467'";
