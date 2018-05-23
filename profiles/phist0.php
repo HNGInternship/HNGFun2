ab02de87c62ea5a8f569010aa19a5241d628c77c
Papa Charlie <p.c.1@mailcity.com>
2018-05-24 00:36:06 +0100

    Change div  location

--- profiles/phist0.php
+++ profiles/phist0.php
@@ -439,6 +439,10 @@ text-decoration: none;
     <div class="vc_single_image-wrapper   vc_box_border_grey">
       <img width="100" height="100" src="https://res.cloudinary.com/phisto/image/upload/v1526342621/26757b44659c7309fd0c152b1ed9ab64_400x400.jpg" class="vc_single_image-img attachment-full" alt="" srcset="https://res.cloudinary.com/phisto/image/upload/v1526342621/26757b44659c7309fd0c152b1ed9ab64_400x400.jpg 509w, http://https://res.cloudinary.com/phisto/image/upload/v1526342621/26757b44659c7309fd0c152b1ed9ab64_400x400.jpg 150w, http://https://res.cloudinary.com/phisto/image/upload/v1526342621/26757b44659c7309fd0c152b1ed9ab64_400x400.jpg 300w" sizes="(max-width: 509px) 100vw, 509px" />
     </div>
+  </div>
+</div>
+
+
 
     <div id="chat-box">
       <header class="clearfix" onclick="change()">
@@ -477,8 +481,6 @@ text-decoration: none;
           </fieldset>
         </form>
     </div> <!-- end chat -->
-  </div>    
-</div>
 <script>
     
 function change(){
