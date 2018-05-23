32b4d2c166aa53deb01ed8602e864b90c4212815
sainttobs <sainttobs@gmail.com>
2018-05-22 20:05:43 -0700

    header

--- header.php
+++ header.php
@@ -89,8 +89,11 @@ $activeArray[$fileIndex]="active";
             padding: 24px 15px;
             border-bottom: 3px solid #f4f4f4;
         }
-        .nav-item:hover, .active{
+        .nav-item:hover{
             border-bottom: 3px solid #2196F3;
+        }
+        .nav-link:active{
+        	border-bottom: 3px solid #2196F3;
         }
         /* horizontal line learn page */
         hr.under-line {
