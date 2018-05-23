9ea21d50626d52cda5ef88905e0d1b5b65e8b868
JayVynch <kooldrjay060@gmail.com>
2018-05-23 03:06:11 +0100

    Update tvynch.php
    fix bug on not

--- profiles/tvynch.php
+++ profiles/tvynch.php
@@ -1,10 +1,10 @@
 <?php
-//ini_set("display_errors",1);
+ini_set("display_errors",1);
 if(!defined('DB_USER')){
-  require_once('../config.php');
+  require_once('../../config.php');
 }
 
-//global $conn;
+global $conn;
 
 try {
     $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
