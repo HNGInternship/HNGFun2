56baa922b7abbcb11fa488b8956e8e71b8f919f1
realtech <aiboroma467@users.noreply.github.com>
2018-05-23 10:50:42 +0100

    Update dreamtech467.php

--- profiles/dreamtech467.php
+++ profiles/dreamtech467.php
@@ -1,20 +1,13 @@
 
 <?php
-	if(!defined('DB_USER')){
-		if (file_exists('../../config.php')) {
-			require_once '../../config.php';
-		} else if (file_exists('../config.php')) {
-			require_once '../config.php';
-		} elseif (file_exists('config.php')) {
-			require_once 'config.php';
-		}
-			
-		try {
-			$conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);			
-		} catch (PDOException $e) {
-			die("Could not connect to the database " . DB_DATABASE . ": " . $e->getMessage());
-		}
-	}
+if(!defined('DB_USER')){
+     require "../../config.php";
+     try {
+         $conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);
+     } catch (PDOException $pe) {
+         die("Could not connect to the database " . DB_DATABASE . ": " . $pe->getMessage());
+     }
+   }
 
   try {
       $sql = "SELECT * FROM interns_data WHERE username ='dreamtech467'";
@@ -45,21 +38,6 @@
 		//chatBot
 	if($_SERVER['REQUEST_METHOD'] === "POST"){
 		
-		if(!defined('DB_USER')){
-		if (file_exists('../../config.php')) {
-			require_once '../../config.php';
-		} else if (file_exists('../config.php')) {
-			require_once '../config.php';
-		} elseif (file_exists('config.php')) {
-			require_once 'config.php';
-		}
-			
-		try {
-			$conn = new PDO("mysql:host=". DB_HOST. ";dbname=". DB_DATABASE , DB_USER, DB_PASSWORD);			
-		} catch (PDOException $e) {
-			die("Could not connect to the database " . DB_DATABASE . ": " . $e->getMessage());
-		}
-	}
 		
 		
 		
