e28c0607860b8308d51680af5ab72292b1a14ad9
realtech <aiboroma467@users.noreply.github.com>
2018-05-24 07:20:12 +0100

    Update dreamtech467.php

--- profiles/dreamtech467.php
+++ profiles/dreamtech467.php
@@ -140,6 +140,8 @@
 		<meta name="viewport" content="width=device-width, initail-scale=1">
 		<title>Abraham Profile</title>
 		
+		
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://code.jquery.com/jquery-git.min.js"></script>
 		<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 		<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
@@ -181,7 +183,7 @@
 					
 				  $.ajax({
 						url: "/profiles/dreamtech467.php",
-						type: "get",
+						type: "post",
 						data: {message: message},
 						dataType: "json",
 						success: function(response){
