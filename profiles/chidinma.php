212c362ab2bba966283343fa4f02ff5633ed6382
chidinma <chypearlnel@gmail.com>
2018-05-24 12:02:14 +0100

    hey

--- profiles/chidinma.php
+++ profiles/chidinma.php
@@ -296,7 +296,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
             
             }
         };
-        xhttp.open("POST", "profiles/chidinma.php");
+        xhttp.open("POST", "chidinma.php");
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhttp.send("message="+message.value);
     }
