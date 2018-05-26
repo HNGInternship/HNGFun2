3f4d5bf96dd9781e41af2df78050f85bcd8af8de
realtech <aiboroma467@users.noreply.github.com>
2018-05-26 22:18:33 +0100

    Update figma.php

--- our-partners/figma.php
+++ our-partners/figma.php
@@ -86,19 +86,19 @@
 							<div class="row">
 								<div class="col-md-12 mt-3 cont1">
 									<h3 style="border-bottom: 5px solid #2196F3; display: inline block; width:70px; "> Contributions</h3>
-
-									<p>The Figma team with its passion for design education is keen on crafting a one-stop shop for all design needs. With much than than regular interface design, local font integration, multi-device catering, rapid prototyping, and real-time collaboration. Figma is one of the Software tool partners for HNG internship 4.0, and as such figma -the tool - is the official design tool for the HNG 4 internship.</p>
+ 
+									<p class="text-justify">The Figma team with its passion for design education is keen on crafting a one-stop shop for all design needs. With much than than regular interface design, local font integration, multi-device catering, rapid prototyping, and real-time collaboration. Figma is one of the Software tool partners for HNG internship 4.0, and as such figma -the tool - is the official design tool for the HNG 4 internship.</p>
 
 									<ul class="listStyle">
-									<li class="mt-3">
-										<span>Figma has empowered the HNG 4.0 teams to stay focused with a tight feedback loop that ensures all changes and updates are synced instantly.</span>
-							
-									</li>
-									<li class="mt-3">
-										<span>Figms has been dedicated to ensuring that talents youths in Africa are given the opportunity to maximize their potentials</span>
-							
-									</li>
-									</ul>  
+										<li class="mt-3">
+											<span class="text-justify">Figma has empowered the HNG 4.0 teams to stay focused with a tight feedback loop that ensures all changes and updates are synced instantly.</span>
+								
+										</li>
+										<li class="mt-3">
+											<span class="text-justify">Figms has been dedicated to ensuring that talents youths in Africa are given the opportunity to maximize their potentials</span>
+								
+										</li>
+									</ul>
 
 								</div>
 								
