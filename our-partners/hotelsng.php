1da97423a10b0c6e0c7c2d8dc332d23a1c3b30b0
realtech <aiboroma467@users.noreply.github.com>
2018-05-29 10:37:37 +0100

    Update hotelsng.php

--- our-partners/hotelsng.php
+++ our-partners/hotelsng.php
@@ -11,13 +11,34 @@
 					<div class="row ">
 						<div class="col-md-4 bder" >
 							<div class="thumbnail headImg">
-								<button type="button" class="btn">VISIT WEBSITE</button>
+								<a class="btn btnimg" href="https://hng.fun/index" role="button">VISIT WEBSITE</a>
 							</div>
 							
 						</div>
-						<div class="col-md-7 headTxt bder">
-							<h3>Hotels.ng <img src="../img/value.png" width="67px" height="87px"> <span class="circle"></span> </img></h3>
-							<h6>Yaba, Lagos, Nigeria</h6>
+						<div class="col-md-6 headTxt bder">
+						
+						
+							<ul class="list-inline">
+							  <li class="list-inline-item copyleft mt-3 mt-0">
+								<h3>Hotels.ng 
+									
+								</h3>
+								
+							  <h6>Yaba, Lagos, Nigeria</h6>
+							  </li>
+							  <li class="list-inline-item copyleft  ">
+								<div class="tear">
+									<div class="circle mt-1 ml-1"></div>
+								</div>
+							  </li>
+							  <li class="list-inline-item copyright">
+								 
+							  </li>
+							 </ul>
+					
+						
+							
+							
 						</div>
 					
 					
@@ -29,12 +50,13 @@
 	
 	
 	<section class="mx-auto col-md-10 mt-5 mb-5 head2">
+	
 		<div class="row">
 			<div class="col-md-4 bder">
 				<div class="cont voffset">
 						<h3 style="border-bottom: 5px solid #2196F3; display: inline block; width:70px; ">About</h3>
 
-						<p class=""> An online travel agency specialising in hotel bookings withing Nigeria. 
+						<p class="text-justify"> An online travel agency specialising in hotel bookings withing Nigeria. 
 							The HNG Internship program was started by Hotels.ng CEO Mark Essien and was designed to 
 							locate the most talented software developers in Nigeria and Africa as a whole. Creating an 
 							interactive platform for software development leanrning that is both fun and rewarding.</p>
@@ -70,37 +92,42 @@
 			</div>
 								
 			<div class="col-md-7 bder">
-							<div class="row mt-5 cont1" >
-								
-									<div class="col" >
-										<img src="../img/hng-square.png">
-									</div> 
-
-									<div class="col-sm-3 bder">
-										<span class="listing"><h6>Headquarters:</h6>Yaba, Lagos</span>
-										<span class="listing"><h6>Address:</h6> No 3, Birrel Avenue</span>  
-										<span class="listing"><h6>Industry:</h6>Hotel Booking Services</span>
-									</div>
-									
-									<div class="col-sm-3 bder">
-										<div class="listing"><h6>Phone:</h6>0806 621 2033</div>
-										<div class="listing"><h6>Founded:</h6>1972</div> 
-									</div>
-								
+						
+						<div class="row" >
+								<div class="col-md-12 mt-3 cont1">
+									<ul class="list-inline">
+									  <li class="list-inline-item copyleft">
+										<img src="../img/hng-square.png" class="smImg">
+									  </li>
+									  <li class="list-inline-item copyleft">
+										<h6 class="mb-0">Headquarters:</h6><span class="mb-1 text-muted">Yaba, Lagos</span>
+										<h6 class="mb-0 mt-2">Address:</h6> <span class="mb-1 text-muted"> No 3, Birrel Avenue</span>
+										<h6 class="mb-0 mt-2">Industry:</h6> <span class="mb-1 text-muted">Hotel Booking Services</span>
+									  </li>
+									  <li class="list-inline-item copyleft">
+										<div><h6 class="mb-0">Phone:</h6><span class="mb-1 text-muted">0806 621 2033</span></div>
+										<div><h6 class="mb-0">Founded:</h6><span class="mb-1 text-muted">1972</span></div> 
+									  </li>
+									</ul>
+								</div>
 							</div>
+			
 							<div class="row">
 								<div class="col-md-12 mt-5 cont1">
 									<h3 style="border-bottom: 5px solid #2196F3; display: inline block; width:70px; "> Contributions</h3>
 
 									
-									<p>Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials</p>
+									<p class="text-justify">Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials</p>
 
 									<ul class="listStyle">
-									<li class="mt-3"><span>Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials</span>
+									<li>
+										<span class="text-justify">Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials</span>
 									</li>
-									<li class="mt-3"><span>Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials</span>
+									<li>
+										<span class="text-justify">Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials</span>
 									</li>
-									<li class="mt-3"><span>Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials</span>
+									<li>
+										<span class="text-justify">Hotels.ng has been a major partner in HNG internships with the dollpwing contributions, they are dedicated to ensuring that talentes youths in Africa aregiven the opportunity to maximize their potentials</span>
 									</li>
 									</ul>  
 
