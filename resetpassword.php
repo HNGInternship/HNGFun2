bc1fa86ee25b7fa68cfe2e7355228462efe902e8
Epospiky <epospiky@gmail.com>
2018-06-02 11:06:54 +0100

    modified reset password

--- resetpassword.php
+++ resetpassword.php
@@ -4,24 +4,24 @@ include_once("header.php");
 <style>
     
 </style>
+<div  class="container" >
 
-<div class="container" >
+<div class="row justify-content-md-center" >
     
-<div class="clearfix"></div>
-    <div style="500px" class="justify-content-md-center">
+    <div style="border:0px;" class=" col-md-4 ">
         
-        <div id="notif" style = "text-align:center;"></div>
+        <h6 id="notif" style = "text-align:center;"></h6>
 
         <form  id="reset_form" style ="text-align: center;">
             <h1>Reset Password</h1>
-            <p >
+            <p style="font-size:16px">
                 Enter your email address and we'll send you an email with instructions to reset your password.
             </p>
             <div style="padding: 20px 50px 0px 50px;">
                 <input type="email" class="form-control form-control-lg rounded-right" placeholder="johndoe@example.com" aria-label="Username" aria-describedby="basic-addon1" id="email" name="email">
                 <br />
                 <input type="hidden" name="reset_password_token" value="yes">
-                <button id='reset' class="btn btn-primary btn-block" type="submit" style="border-radius: 8px;">Reset Password</button>
+                <button id='reset' class="btn btn-primary btn-block" type="submit" style="border-radius: 8px; ">Reset Password</button>
                 <p style='color: #ADADAD '>
                      Already have account? <a href="login.php" style="text-decoration: none; "><span style="color: #1E99E0">Log In</span></a> 
                 </p>
@@ -29,7 +29,7 @@ include_once("header.php");
         </form>
     </div> 
 </div>
-
+</div>
 <script type="text/javascript">
        $( document ).ready(function() {
     $("#reset").click(function(e){
@@ -39,7 +39,7 @@ include_once("header.php");
         var email = $("#email").val();
              
         if(email ==""){
-            $("#notif").addClass(' alert alert-danger');
+            $("#notif").addClass(' text-danger');
             $("#notif").html('Please enter email');
         }
        
@@ -59,13 +59,13 @@ include_once("header.php");
             success: function(data){
             if(data=="1"){
              
-             $("#notif").addClass('alert alert-success');
+             $("#notif").addClass('text-success');
             $("#notif").html("Success! Please click on the link in the email to set a new password. ");
 
             $("#reset").html('DONE');      
             }
              else if(data =="0"){
-                 $("#notif").addClass('alert alert-danger'); 
+                 $("#notif").addClass('text-danger'); 
                  $("#notif").html("Account does not exist!");
                  $("#reset").html('Reset Password');
                  }
@@ -82,7 +82,7 @@ include_once("header.php");
             },
             beforeSend :function(){
 
-            $("#notif").removeClass('alert alert-danger');
+            $("#notif").removeClass('text-danger');
             $("#notif").html(' ');
 
             $("#reset").html('Requesting..');
