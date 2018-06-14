407ee7f32f36701b7e95e1587f7945381a877ca5
wekesamuel <wekesamuel@yahoo.com>
2018-06-14 15:23:55 +0100

    core_values

--- core_values.php
+++ core_values.php
@@ -19,14 +19,14 @@ include_once("header.php");
       <div class="col-lg-5 col-md-6 col-sm-12">
         <center>
           <img src="img/teamwork1.png" class="img-responsive">
-          <h2 style="font-size:20px">TEAM WORK</h2>
+          <h2 style="font-size:20px; padding-top:30px;">TEAM WORK</h2>
           <p>We help each other, we collaborate, we celebrate our achievements – and we have fun doing it!</p>
         </center>
       </div>
       <div class="col-lg-5 col-md-6 col-sm-12">
         <center>
           <img src="img/team.png" class="img-responsive">
-          <h2 style="font-size:20px">COMMUNITY</h2>
+          <h2 style="font-size:20px; padding-top:30px;">COMMUNITY</h2>
           <p>We share an infectious sense of mission to make an impact on society, empowering youths in ways never before possible.</p>
         </center>
       </div>
@@ -38,14 +38,14 @@ include_once("header.php");
       <div class="col-lg-5 col-md-6 col-sm-12">
         <center>
           <img src="img/game-controller.png" class="img-responsive">
-          <h2 style="font-size:20px">FUN</h2>
+          <h2 style="font-size:20px; padding-top:30px;">FUN</h2>
           <p>We believe humor is essential for exceptional creativity, outstanding service and thriving team members</p>
         </center>
       </div>
       <div class="col-lg-5 col-md-6 col-sm-12">
         <center>
           <img src="img/gold-medal.png" class="img-responsive">
-          <h2 style="font-size:20px">EXCELLENCE</h2>
+          <h2 style="font-size:20px; padding-top:30px;">EXCELLENCE</h2>
           <p>We are committed to winning with integrity by giving attention to every detail. </p>
         </center>
       </div>
@@ -57,7 +57,7 @@ include_once("header.php");
       <div class="col-lg-6 col-md-6 col-sm-12">
         <center>
           <img src="img/idea.png" class="img-responsive">
-          <h2 style="font-size:20px">INNOVATION</h2>
+          <h2 style="font-size:20px; padding-top:30px;">INNOVATION</h2>
           <p>We thrive on creativity and ingenuity.</p>
         </center>
       </div>
