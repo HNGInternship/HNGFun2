8f22b9f306db6744cd15dd58528230f02e28e0b4
Osumgba Chiamaka <31241128+osumgbachiamaka@users.noreply.github.com>
2018-06-15 16:41:05 +0100

    Update sponsors.php

--- sponsors.php
+++ sponsors.php
@@ -18,7 +18,7 @@ include_once("header.php");
 
         <div class="d-flex justify-content-center">
            <div class="d-block w-80 partner-link">
-              <a href="our-partners/figma.php"><div class="row border rounded ">
+              <div class="row border rounded ">
                   <div class="col-3 pl-0">
                       <img src="img/figma-dark.png" alt="figma" srcset="" class="img-fluid ">
                   </div>
@@ -26,9 +26,9 @@ include_once("header.php");
                       <h5 class="sponsorbg-text font-weight-bold">Figma Design</h5>
                       <p class="sponsor-subbg-text mt-0 pt-0 f-1">Figma is keen on crafting a one-stop shop for all design needs. With rapid prototyping, and real-time collaboration, Figma empowers teams to stay focused with a tight feedback loop that ensures all changes and updates are synced instantly.</p>
                   </div>
-              </div></a><br>
+              </div><br>
 
-              <a href="our-partners/oracle.php"><div class="row border rounded ">
+              <div class="row border rounded ">
                   <div class="col-3 pl-0">
                       <img src="img/oracle-red.png" alt="oracle" srcset="" class="img-fluid ">
                   </div>
@@ -37,9 +37,9 @@ include_once("header.php");
                       <h5 class="sponsorbg-text font-weight-bold">Oracle</h5>
                       <p class="sponsor-subbg-text mt-0 pt-0 f-1">Oracle remains a visionary in the world of databases. Oracle commits to educating developer teams on how best to design and deploy mission-critical solutions. Support for the HNG internship is just one of their educational outreaches globally.</p>
                   </div>
-              </div></a><br>
+              </div><br>
 
-              <a href="our-partners/bluechip_technologies.php"><div class="row border rounded ">
+              <div class="row border rounded ">
                   <div class="col-3 pl-0">
                       <img src="img/bluechips.png" alt="blue chip technologies" srcset="" class="img-fluid ">
                   </div>
@@ -48,9 +48,9 @@ include_once("header.php");
                       <h5 class="sponsorbg-text font-weight-bold">Blue Chip Technologies</h5>
                       <p class="sponsor-subbg-text mt-0 pt-0 f-1">The go-to firm for robust client data management solutions, Blue chip Technologies remains focused on driving business performance forward through the intricate leverage of strategy, people, process, metrics, applications, data, and architecture.</p>
                   </div>
-              </div></a><br>
+              </div><br>
 
-              <a href="our-partners/hotelsng.php"><div class="row border rounded ">
+              <div class="row border rounded ">
                   <div class="col-3 pl-0">
                       <img src="img/hng-square.png" alt="hotels.ng" srcset="" class="img-fluid ">
                   </div>
@@ -59,7 +59,7 @@ include_once("header.php");
 
                       <p class="sponsor-subbg-text mt-0 pt-0 f-1">Hotels.ng has risen to become the *de facto* application for comparing hotel quality and price. Hotels.ng has spearheaded 4 remote internship schemes that have empowered budding developers to grow their portfolio and value across board.</p>
                   </div>
-              </div></a><br>
+              </div><br>
            </div>
         </div>
 
