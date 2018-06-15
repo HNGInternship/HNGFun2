c90a5581ec419d7996b15845f21df3dc9bf77c1e
oriechinedu <oriechinedu>
2018-06-15 19:47:35 +0100

    fix email

--- process.php
+++ process.php
@@ -188,11 +188,11 @@ if(isset($_POST['login'])){
 
 				//send the message, check for errors
 				
-				if (! mail($email, 'Password Reset', $Body) {
+				if (mail($email, 'Password Reset', $Body, 'hnginternship@hotels.ng') {
 				    //echo "Mailer Error: " . $mail->ErrorInfo;
-				    echo '0';
+				    echo '1';
 				} else {
-				    echo "1";
+				    echo "0";
 
 				}
       		
