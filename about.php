7cbc16eef533b67d4bedae6a6882d3b0c26f40f9
Osumgba Chiamaka <31241128+osumgbachiamaka@users.noreply.github.com>
2018-06-17 01:38:09 +0100

    Africa not Nigeria

--- about.php
+++ about.php
@@ -109,7 +109,7 @@ include_once("header.php");
         <img src="./img.jpg" style='opacity:0.2;width:120%;min-width:1280px;height:auto;min-height:100%;position:absolute;top:-70%;left:-10%' lm me===fgfuwreIGAhwgiv=alt="Background">
 		<div>
 			<h1 class="mb-2">What is HNG all about?</h1>
-			<p style='margin:0;line-height:2'>The HNG is a 3-month remote internship program designed to locate the most talented software developers in Nigeria and the whole of Africa</p>
+			<p style='margin:0;line-height:2'>The HNG is a 3-month remote internship program designed to locate the most talented software developers in Africa</p>
 		</div>
 	</div>
 	<div class="row">
@@ -123,8 +123,8 @@ include_once("header.php");
 			<img src="icons/logo-blue-bg.png" style="max-width: 300px; max-height: 252px;">
 		</div> -->
 		<div class="content">
-			<p class="text-muted px-5"> The HNG is a 3-month remote internship program designed to locate the most talented software developers in Nigeria
-            and the whole of Africa. Everyone is welcome to participate (there is no entrance exam). We create fun challenges every week on our slack channel.
+			<p class="text-muted px-5"> The HNG is a 3-month remote internship program designed to locate the most talented software developers in
+            Africa. Everyone is welcome to participate (there is no entrance exam). We create fun challenges every week on our slack channel.
             Those who solve them stay on. Everyone gets to learn important concepts quickly, and make connections with people they can work with in the future.
             The intern coders are introduced to complex programming frameworks, and get to work on real applications that scale. the finalist are connected to
             the best companies in the tech ecosystem and get full time jobs and contracts immediately </p>
